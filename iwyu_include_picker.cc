66779c715ef1d91c3a071baf1cc703a47fbc0696
csilvers+iwyu <csilvers+iwyu@google.com>
2011-05-04 18:18:43 +0000

    For some reason clang doesn't see stdarg.h as providing va_list.  stdio.h seems to, at least for gcc, via a check against _VA_LIST_DEFINED, so I leave stdio.h as an acceptable place to get va_list as well.
    R=dsturtevant
    DELTA=2  (2 added, 0 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=1721

--- iwyu_include_picker.cc
+++ iwyu_include_picker.cc
@@ -155,6 +155,8 @@ const IncludePicker::IncludeMapEntry symbol_include_map[] = {
   // add them in by hand as I discover them.
   { "EOF", kPrivate, "<stdio.h>", kPublic },
   { "EOF", kPrivate, "<libio.h>", kPublic },
+  { "va_list", kPrivate, "<stdarg.h>", kPublic },
+  { "va_list", kPrivate, "<stdio.h>", kPublic },
   // These are symbols that could be defined in either stdlib.h or
   // malloc.h, but we always want the stdlib location.
   { "malloc", kPrivate, "<stdlib.h>", kPublic },
