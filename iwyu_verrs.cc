d6d8ac9ccfde3a1d467b7521bfb6812afa56764e
csilvers+iwyu <csilvers+iwyu@google.com>
2011-12-01 02:46:29 +0000

    Blah, I didn't realize this third file had been added too.
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=3896

---
+++ iwyu_verrs.cc
@@ -0,0 +1,44 @@
+//===--- iwyu_verrs.h - debug output for include-what-you-use ----===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "iwyu_verrs.h"
+
+#include "iwyu_globals.h"
+#include "iwyu_location_util.h"
+#include "iwyu_path_util.h"
+
+namespace include_what_you_use {
+
+using clang::FileEntry;
+
+namespace {
+int verbose_level = 1;
+}  // namespace
+
+void SetVerboseLevel(int level) {
+  verbose_level = level;
+}
+
+int GetVerboseLevel() {
+  return verbose_level;
+}
+
+bool ShouldPrintSymbolFromFile(const FileEntry* file) {
+  if (GetVerboseLevel() < 5) {
+    return false;
+  } else if (GetVerboseLevel() < 10) {
+    return ShouldReportIWYUViolationsFor(file);
+  } else if (GetVerboseLevel() < 11) {
+    return !IsSystemIncludeFile(GetFilePath(file));
+  } else {
+    return true;
+  }
+}
+
+}  // namespace include_what_you_use
