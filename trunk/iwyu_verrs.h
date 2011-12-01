b01eb00e301bdf3ac3471cdfa0fc9bf36aca1f1f
csilvers+iwyu@google.com <csilvers+iwyu@google.com@812d1f90-f944-d88a-411a-122bdc93e1ea>
2011-12-01 02:37:46 +0000

    Oops, forgot to submit these new files to SVN.
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=3895

---
+++ trunk/iwyu_verrs.h
@@ -0,0 +1,45 @@
+//===--- iwyu_verrs.h - debug output for include-what-you-use ----===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// This module controls logging and verbosity levels for include-what-you-use.
+
+#ifndef DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_IWYU_VERRS_H_
+#define DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_IWYU_VERRS_H_
+
+#include "llvm/Support/raw_ostream.h"
+
+namespace clang {
+class FileEntry;
+}
+
+namespace include_what_you_use {
+
+void SetVerboseLevel(int level);
+int GetVerboseLevel();
+
+// Returns true if we should print a message at the given verbosity level.
+inline bool ShouldPrint(int verbose_level) {
+  return verbose_level <= GetVerboseLevel();
+}
+
+// Returns true if we should print information about a symbol in the
+// given file, at the current verbosity level.  For instance, at most
+// normal verbosities, we don't print information about symbols in
+// system header files.
+bool ShouldPrintSymbolFromFile(const clang::FileEntry* file);
+
+// VERRS(n) << blah;
+// prints blah to errs() if the verbose level is >= n.
+#define VERRS(verbose_level) \
+  if (!::include_what_you_use::ShouldPrint( \
+          verbose_level)) ; else ::llvm::errs()
+
+}  // namespace include_what_you_use
+
+#endif  // DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_IWYU_VERRS_H_
