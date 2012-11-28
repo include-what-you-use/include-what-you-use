09aff0f29761b700449902ba10eca03b3d52ccfe
Volodymyr Sapsai <vsapsai@users.noreply.github.com>
2012-11-28 09:41:45 +0000

    Update to reflect changes in Clang. Patch by Kim Gräsman.
    In Clang r168703 was changed arguments list of clang::driver::Driver constructor.

--- iwyu_driver.cc
+++ iwyu_driver.cc
@@ -161,8 +161,7 @@ CompilerInstance* CreateCompilerInstance(int argc, const char **argv) {
   IntrusiveRefCntPtr<DiagnosticIDs> diagnostic_id(new DiagnosticIDs());
   DiagnosticsEngine diagnostics(diagnostic_id, &*diagnostic_options,
                                 diagnostic_client);
-  Driver driver(path.str(), getDefaultTargetTriple(), "a.out", false,
-                diagnostics);
+  Driver driver(path.str(), getDefaultTargetTriple(), "a.out", diagnostics);
   driver.setTitle("include what you use");
 
   // Expand out any response files passed on the command line
