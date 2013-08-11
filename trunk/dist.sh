afa1fc4abb94cad861afbeedb207daf96a85a109
kim.grasman@gmail.com <kim.grasman@gmail.com@812d1f90-f944-d88a-411a-122bdc93e1ea>
2013-08-11 20:23:59 +0000

    Distribution shell script.

---
+++ trunk/dist.sh
@@ -0,0 +1,28 @@
+##===--------- dist.sh - package up binary distribution of IWYU -----------===##
+#
+#                      The LLVM Compiler Infrastructure
+#
+# This file is distributed under the University of Illinois Open Source
+# License. See LICENSE.TXT for details.
+#
+##===----------------------------------------------------------------------===##
+
+# Create clean dist directory
+rm -rf dist
+mkdir dist
+
+# Assume that Release+Asserts build exists.
+ln -f ../../../../../build/Release+Asserts/bin/include-what-you-use dist/
+ln -f fix_includes.py dist/
+ln -f README.txt dist/
+ln -f LICENSE.TXT dist/
+
+pushd dist > /dev/null
+
+# Build archive name to the following pattern:
+# include-what-you-use-<ver>-<arch>-<distro>-<release>.tar.gz
+ARCHIVE=include-what-you-use-3.3-$(uname -m)-$(lsb_release -si)-$(lsb_release -sr).tar.gz
+tar -cvzf $ARCHIVE *
+
+popd > /dev/null
+
