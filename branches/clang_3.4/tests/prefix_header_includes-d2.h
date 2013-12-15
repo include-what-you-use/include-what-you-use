16ee65502fcb3c91ad187b2605a1926ad447da07
vsapsai@gmail.com <vsapsai@gmail.com@812d1f90-f944-d88a-411a-122bdc93e1ea>
2013-12-15 14:07:43 +0000

    Merging from trunk to clang_3.4 branch r497: ------------------------------------------------------------------------ r497 | vsapsai@gmail.com | 2013-12-15 14:33:18 +0200 (Sun, 15 Dec 2013) | 5 lines
    Add --prefix_header_includes option (fixes issue #112).
    
    --prefix_header_includes controls presence of includes and forward declarations
    involving files included via command-line option -include. Issue is reported by
    Max Dyckhoff.
    ------------------------------------------------------------------------

---
+++ branches/clang_3.4/tests/prefix_header_includes-d2.h
@@ -0,0 +1,17 @@
+//===--- prefix_header_includes-d2.h - test input file for iwyu -----------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_TESTS_PREFIX_HEADER_INCLUDES_D2_H_
+#define DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_TESTS_PREFIX_HEADER_INCLUDES_D2_H_
+
+#include "tests/prefix_header_includes-i1.h"
+
+class CommandLineIncludeD2 {};
+
+#endif  // DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_TESTS_PREFIX_HEADER_INCLUDES_D2_H_
---
+++ branches/clang_3.4/tests/prefix_header_includes-d4.h
@@ -0,0 +1,15 @@
+//===--- prefix_header_includes-d4.h - test input file for iwyu -----------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_TESTS_PREFIX_HEADER_INCLUDES_D4_H_
+#define DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_TESTS_PREFIX_HEADER_INCLUDES_D4_H_
+
+class CommandLineIncludeD4 {};
+
+#endif  // DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_TESTS_PREFIX_HEADER_INCLUDES_D4_H_
