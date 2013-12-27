78b3e6e22a36273e192f39a3ac457f8bee5f7175
kim.grasman@gmail.com <kim.grasman@gmail.com@812d1f90-f944-d88a-411a-122bdc93e1ea>
2013-12-27 16:25:27 +0000

    Explicitly handle new CK_AddressSpaceConversion cast kind to get rid of warning. Address space conversion casts can only happen between pointers, so no need for full type info, and thereby no need for IWYU analysis.

--- trunk/iwyu.cc
+++ trunk/iwyu.cc
@@ -1912,6 +1912,9 @@ class IwyuBaseAstVisitor : public BaseAstVisitor<Derived> {
       case clang::CK_ReinterpretMemberPointer:
       case clang::CK_BuiltinFnToFnPtr:
       case clang::CK_ZeroToOCLEvent: // OpenCL event_t is a built-in type.
+      case clang::CK_AddressSpaceConversion:  // Address spaces are associated
+                                              // with pointers, so no need for
+                                              // the full type.
         break;
 
       // We shouldn't be seeing any of these kinds.
