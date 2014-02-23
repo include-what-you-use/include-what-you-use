8821f57d94387aeaa9c5d7e8a6acab85cc39e2ae
Volodymyr Sapsai <vsapsai@users.noreply.github.com>
2014-02-23 00:31:52 +0000

    Handle --version option (issue #106).

---
+++ iwyu_version.h
@@ -0,0 +1,15 @@
+//===--- iwyu_version.h - include-what-you-use versioning -----------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_IWYU_VERSION_H_
+#define DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_IWYU_VERSION_H_
+
+#define IWYU_VERSION_STRING "0.2"
+
+#endif  // DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_IWYU_VERSION_H_
