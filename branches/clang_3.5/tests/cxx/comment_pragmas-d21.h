8fab1c107db4448b8124322273e1aa0751bc5f29
kim.grasman@gmail.com <kim.grasman@gmail.com@812d1f90-f944-d88a-411a-122bdc93e1ea>
2014-10-05 20:23:58 +0000

    Merge from trunk to clang_3.5 branch r586: ------------------------------------------------------------------------ r586 | kim.grasman@gmail.com | 2014-10-05 21:38:22 +0200 (Sun, 05 Oct 2014) | 9 lines
    Fix issue 140: pragma keep doesn't keep duplicates
    
    The removal of duplicates is now countered by recording the included FileEntry
    of all include lines marked "IWYU pragma: keep", and then marking all includes
    of said FileEntry as desired.
    
    We think that if the user goes to the trouble of marking an include line with
    the keep pragma, they can probably explicitly remove additional includes of
    that header manually if they're not wanted.
    ------------------------------------------------------------------------

---
+++ branches/clang_3.5/tests/cxx/comment_pragmas-d21.h
@@ -0,0 +1,15 @@
+//===--- comment_pragmas-d21.h - test input file for iwyu -----------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_TESTS_COMMENT_PRAGMAS_D21_H_
+#define DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_TESTS_COMMENT_PRAGMAS_D21_H_
+
+class CommentPragmasD21 {};
+
+#endif  // DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_TESTS_COMMENT_PRAGMAS_D21_H_
