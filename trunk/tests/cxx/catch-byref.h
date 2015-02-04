40cc59eaa06b14d40c60f586d7a2f395aa094a0d
kim.grasman@gmail.com <kim.grasman@gmail.com@812d1f90-f944-d88a-411a-122bdc93e1ea>
2015-02-04 20:54:21 +0000

    Fix issue 171: Support catch statements.
    Catching a type requires that it's complete. This was found by one of my
    co-workers when we ran IWYU over our ~1MLOC code base.

---
+++ trunk/tests/cxx/catch-byref.h
@@ -0,0 +1,15 @@
+//===--- catch-byref.h - test input file for iwyu -------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_CATCH_BYREF_H_
+#define DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_CATCH_BYREF_H_
+
+class CatchByRef {};
+
+#endif // DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_CATCH_BYREF_H_
