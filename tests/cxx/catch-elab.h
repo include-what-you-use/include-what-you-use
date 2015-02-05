9c52686980bad578d0eb3b6211dacd798a2a1e2f
Kim Gräsman <kim@mvps.org>
2015-02-05 19:25:11 +0000

    Add test for elaborated exception types.

---
+++ tests/cxx/catch-elab.h
@@ -0,0 +1,17 @@
+//===--- catch-elab.h - test input file for iwyu --------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_CATCH_ELAB_H_
+#define DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_CATCH_ELAB_H_
+
+namespace Namespace {
+class CatchElab {};
+}
+
+#endif // DEVTOOLS_MAINTENANCE_INCLUDE_WHAT_YOU_USE_CATCH_ELAB_H_
