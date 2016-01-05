4184ff1c309e38b042b3adb8bb92d8d01f96c444
Kim Grasman <kim.grasman@gmail.com>
2016-01-05 20:12:49 +0100

    Suppress Windows GUI prompts for critical errors

--- more_tests/test_main.cc
+++ more_tests/test_main.cc
@@ -1,8 +1,31 @@
 #include "iwyu_globals.h"
 #include "gtest/gtest.h"
 
+#if defined(_WIN32)
+#include <windows.h>
+#if defined(_MSC_VER)
+#include <crtdbg.h>
+#endif  // _MSC_VER
+#endif  // _WIN32
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   include_what_you_use::InitGlobalsAndFlagsForTesting();
+
+#if defined(_WIN32)
+  // Windows reports critical errors using GUI prompts by default,
+  // force it not to.
+  ::SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
+#if defined(_MSC_VER)
+  ::_set_error_mode(_OUT_TO_STDERR);
+  _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
+  _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
+  _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
+  _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
+  _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
+  _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
+#endif  // _MSC_VER
+#endif  // _WIN32
+
   return RUN_ALL_TESTS();
 }
