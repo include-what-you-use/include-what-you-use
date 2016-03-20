9b40503d5b54f8911eb1f6ec5d97f3a6d7f94ea5
Kim Grasman <kim.grasman@gmail.com>
2016-03-20 15:49:33 +0100

    Fix IsInScratchSpace so it actually works.
    Scratch space locations look like "<scratch space>:line:col", but we compared
    directly against "<scratch space>", which would always fail.
    
    Move macro-arg-concatenation test from badinc to macro_location to make it
    easier to detect failures in the future.

--- tests/cxx/macro_location.h
+++ tests/cxx/macro_location.h
@@ -27,10 +27,21 @@ CREATE_VAR(HClass);
 // forward-declared by DECLARE_INDIRECT's file.
 DECLARE_INDIRECT(global_indirect);
 
+// Macro-concatenated locations end up in <scratch space>, check that they're
+// properly attributed to expansion.
+#define CONCAT(a, b) a##b
+
+// IWYU: ConcatFwdDeclClass needs a declaration
+CONCAT(Concat, FwdDeclClass) *global_concat_ptr;
+
+// IWYU: ConcatClass is...*macro_location-i4.h
+CONCAT(Concat, Class) global_concat;
+
 /**** IWYU_SUMMARY
 
 tests/cxx/macro_location.h should add these lines:
 #include "tests/cxx/macro_location-i3.h"
+#include "tests/cxx/macro_location-i4.h"
 
 tests/cxx/macro_location.h should remove these lines:
 - #include "tests/cxx/macro_location-d3.h"  // lines XX-XX
@@ -40,6 +51,7 @@ The full include-list for tests/cxx/macro_location.h:
 #include "tests/cxx/indirect.h"  // for IndirectClass
 #include "tests/cxx/macro_location-d2.h"  // for ARRAYSIZE, CREATE_VAR, DECLARE_INDIRECT, NEW_CLASS, USE_CLASS
 #include "tests/cxx/macro_location-i3.h"  // for Foo
+#include "tests/cxx/macro_location-i4.h"  // for ConcatClass, ConcatFwdDeclClass (ptr only)
 
 
 ***** IWYU_SUMMARY */
