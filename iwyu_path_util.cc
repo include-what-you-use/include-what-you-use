08995c1344df46324f51cc9b89ec487f1843bf81
Flamefire <git@grundis.de>
2016-06-06 10:26:21 +0200

    Fix assertion failure for <built-in>

--- iwyu_path_util.cc
+++ iwyu_path_util.cc
@@ -82,6 +82,11 @@ string Basename(const string& path) {
 }
 
 string GetCanonicalName(string file_path) {
+  // For this special 'path' we just return it.
+  // Note that we leave the 'quotes' to make it different from regular paths.
+  if (file_path == "<built-in>")
+    return file_path;
+
   CHECK_(!IsQuotedInclude(file_path));
 
   file_path = NormalizeFilePath(file_path);
