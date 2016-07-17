c8d9e85bb51e6358bb9008b34ad2b998a45f764a
Kim Grasman <kim.grasman@gmail.com>
2016-07-17 11:24:48 +0200

    Add test for anonymous forward decls.

--- tests/cxx/macro_location-i3.h
+++ tests/cxx/macro_location-i3.h
@@ -10,3 +10,22 @@
 #define DECLARE_FRIEND(cls)  friend class cls
 
 class Foo {};
+
+
+// Reduced example from the Linux kernel:
+// http://lxr.free-electrons.com/source/tools/include/linux/compiler.h#L112
+//
+// (The below test code is absolute nonsense, but it triggers the same IWYU
+// behavior as the actual READ_ONCE macro.)
+//
+// This makes sure we don't treat anonymous type declarations as forward-decls
+// in the context of macro use locations.
+#define ANONYMOUS_TYPE_IN_MACRO(value) \
+  char nonsense() {                    \
+    union {                            \
+      int v;                           \
+      char c[1];                       \
+    } u;                               \
+    u.v = value;                       \
+    return u.c[0];                     \
+  }
