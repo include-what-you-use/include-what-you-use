e95e6c371ffb2a1a3a15c9c33caec0ae55f8b7f8
Kim Grasman <kim.grasman@gmail.com>
2017-01-06 10:53:14 +0100

    Update to reflect changes in Clang
    r291184 switched the CompilerInvocation argument to a shared_ptr.

--- iwyu_driver.cc
+++ iwyu_driver.cc
@@ -207,7 +207,7 @@ CompilerInstance* CreateCompilerInstance(int argc, const char **argv) {
   const ArgStringList &cc_arguments = command.getArguments();
   const char** args_start = const_cast<const char**>(cc_arguments.data());
   const char** args_end = args_start + cc_arguments.size();
-  unique_ptr<CompilerInvocation> invocation(new CompilerInvocation);
+  std::shared_ptr<CompilerInvocation> invocation(new CompilerInvocation);
   CompilerInvocation::CreateFromArgs(*invocation,
                                      args_start, args_end, diagnostics);
   invocation->getFrontendOpts().DisableFree = false;
@@ -238,7 +238,7 @@ CompilerInstance* CreateCompilerInstance(int argc, const char **argv) {
   // Create a compiler instance to handle the actual work.
   // The caller will be responsible for freeing this.
   CompilerInstance* compiler = new CompilerInstance;
-  compiler->setInvocation(invocation.release());
+  compiler->setInvocation(invocation);
 
   // Create the compilers actual diagnostics engine.
   compiler->createDiagnostics();
