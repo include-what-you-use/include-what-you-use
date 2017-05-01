b7323abc3e200354a0498435f7107b936fa25004
Kim Grasman <kim.grasman@gmail.com>
2017-05-01 12:55:10 +0200

    Extract function pointer tests out of badinc

---
+++ tests/cxx/funcptrs-d1.h
@@ -0,0 +1,15 @@
+//===--- funcptrs-d1.h - test input file for iwyu -------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef INCLUDE_WHAT_YOU_USE_TESTS_CXX_FUNCPTRS_D1_H_
+#define INCLUDE_WHAT_YOU_USE_TESTS_CXX_FUNCPTRS_D1_H_
+
+#include "tests/cxx/funcptrs-i1.h"
+
+#endif  // INCLUDE_WHAT_YOU_USE_TESTS_CXX_FUNCPTRS_D1_H_
