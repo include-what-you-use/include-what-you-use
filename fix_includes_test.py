b5761cc76869a05ced05e8ab149dfb37dc521349
Christian Venegas <cvenegas@esri.com>
2018-06-11 14:35:21 -0700

    Enchance fix_includes.py to properly scan Allman namespaces
    The fix_includes.py script has been tailored to work for
    namespaces that have { on the same line as the namespace.
    For styles that do not follow this format, namespace
    detection does not work properly as the line numbers and
    regex statement are not able to be evaluated.  Logic has
    been added to detect when an Allman namespace has been
    found and shifts the line numbers appropriately.
    
    Signed-off-by: Christian Venegas <cvenegas@esri.com>

--- fix_includes_test.py
+++ fix_includes_test.py
@@ -339,6 +339,80 @@ namespace maps_transit_realtime { namespace service_alerts { class StaticService
     self.RegisterFileContents({'empty_internal_namespace': infile})
     self.ProcessAndTest(iwyu_output)
 
+  def testRemoveEmptyAllmanNamespace(self):
+    """Tests we remove a namespace with allman braces if we remove all fwd-decls inside it."""
+    infile = """\
+// Copyright 2010
+
+#include <stdio.h>
+
+namespace ns     ///-
+{                ///-
+class Foo;       ///-
+namespace ns2    ///-
+{                ///-
+namespace ns3    ///-
+{                ///-
+class Bar;       ///-
+}                ///-
+}                ///-
+class Baz;       ///-
+}                ///-
+                 ///-
+int main() { return 0; }
+"""
+    iwyu_output = """\
+empty_namespace should add these lines:
+
+empty_namespace should remove these lines:
+- class Foo;  // lines 7-7
+- namespace ns { namespace ns2 { namespace ns3 { class Bar; } } }  // lines 12-12
+- namespace ns { class Baz; } }  // lines 15-15
+
+The full include-list for empty_namespace:
+#include <stdio.h>
+---
+"""
+    self.RegisterFileContents({'empty_namespace': infile})
+    self.ProcessAndTest(iwyu_output)
+
+  def testRemovePartOfEmptyAllmanNamespace(self):
+    """Tests we remove a namespace with allman braces if empty, but not enclosing namespaces."""
+    infile = """\
+// Copyright 2010
+
+namespace maps_transit_realtime
+{
+namespace service_alerts
+{
+class StaticServiceAlertStore;
+namespace trigger                    ///-
+{                                    ///-
+class Trigger;                       ///-
+}  // namespace trigger              ///-
+namespace ui                         ///-
+{                                    ///-
+class Alert;                         ///-
+}  // namespace ui                   ///-
+}  // namespace service_alerts
+}  // namespace maps_transit_realtime
+
+int main() { return 0; }
+"""
+    iwyu_output = """\
+empty_internal_namespace should add these lines:
+
+empty_internal_namespace should remove these lines:
+- namespace maps_transit_realtime { namespace service_alerts { namespace trigger { class Trigger; } } }  // lines 10-10
+- namespace maps_transit_realtime { namespace service_alerts { namespace ui { class Alert; } } }  // lines 14-14
+
+The full include-list for empty_internal_namespace:
+namespace maps_transit_realtime { namespace service_alerts { class StaticServiceAlertStore; } }   // lines 7-7
+---
+"""
+    self.RegisterFileContents({'empty_internal_namespace': infile})
+    self.ProcessAndTest(iwyu_output)
+
   def testRemoveEmptyIfdef(self):
     """Tests we remove an #ifdef if we remove all #includes inside it."""
     # Also makes sure we reorder properly around the removed ifdef.
@@ -2556,6 +2630,32 @@ iterative_namespace should add these lines:
 
 iterative_namespace should remove these lines:
 - class Bar;  // lines 5-5
+
+The full include-list for iterative_namespace:
+---
+"""
+    self.RegisterFileContents({'iterative_namespace': infile})
+    self.ProcessAndTest(iwyu_output)
+
+  def testIterativeAllmanNamespaceDelete(self):
+    """Tests deleting an allman namespace with an emptied #ifdef inside it."""
+    infile = """\
+// Copyright 2010
+                 ///-
+namespace foo    ///-
+{                ///-
+#ifdef FWD_DECL  ///-
+class Bar;       ///-
+#endif           ///-
+}                ///-
+
+int main() { return 0; }
+"""
+    iwyu_output = """\
+iterative_namespace should add these lines:
+
+iterative_namespace should remove these lines:
+- class Bar;  // lines 6-6
 
 The full include-list for iterative_namespace:
 ---
