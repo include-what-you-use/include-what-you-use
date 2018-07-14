e25c11e597e55c050fe7713950bc9348a0e71389
Christian Venegas <cvenegas@esri.com>
2018-07-13 18:23:50 -0700

    Use clang-format style namespace comments for fix_includes.py
    The fix_includes.py script inserts namespace comments for any new
    namespace inclusions.  These comments have two spaces in between
    closing bracket like so:
    
    namespace ns {
    }  // namespace ns
    
    Modify the inclusions to match clang-format style comments for new
    namespace inclusions like so:
    
    namespace ns {
    } // namespace ns
    
    Signed-off-by: Christian Venegas <cvenegas@esri.com>

--- fix_includes.py
+++ fix_includes.py
@@ -1863,11 +1863,11 @@ def _NormalizeNamespaceForwardDeclareLines(lines):
      namespace bar {
      class A;
      class B;
-     }  // namespace bar
+     } // namespace bar
      namespace bang {
      class C;
-     }  // namespace bang
-     }  // namespace foo
+     } // namespace bang
+     } // namespace foo
 
   Non-namespace lines are left alone.  Only adjacent namespace lines
   from the input are merged.
@@ -1891,7 +1891,7 @@ def _NormalizeNamespaceForwardDeclareLines(lines):
     differ_pos = _CommonPrefixLength(namespaces_in_line, current_namespaces)
     namespaces_to_close = reversed(current_namespaces[differ_pos:])
     namespaces_to_open = namespaces_in_line[differ_pos:]
-    retval.extend('}  // namespace %s' % ns for ns in namespaces_to_close)
+    retval.extend('} // namespace %s' % ns for ns in namespaces_to_close)
     retval.extend('namespace %s {' % ns for ns in namespaces_to_open)
     current_namespaces = namespaces_in_line
     # Now add the current line.  If we were a namespace line, it's the
