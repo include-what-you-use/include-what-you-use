102b0520b0c5cff512335a6ef74fb0b38bed202c
Philip Pfaffe <philip.pfaffe@kit.edu>
2018-09-07 21:15:29 +0200

    Address more nits

--- iwyu_tool_test.py
+++ iwyu_tool_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-##===--- iwyu_tool_test.py - test for fix_includes.py ---------------------===##
+##===-------------- iwyu_tool_test.py - test for iwyu_too.py --------------===##
 #
 #                     The LLVM Compiler Infrastructure
 #
@@ -31,7 +31,7 @@ class MockInvocation(iwyu_tool.Invocation):
     def will_return(self, content):
         self._will_return = content
 
-    def run_iwyu(self, verbose):
+    def run(self, verbose):
         if self._will_block > 0:
             time.sleep(self._will_block)
         return self._will_return
@@ -39,59 +39,53 @@ class MockInvocation(iwyu_tool.Invocation):
 
 class IWYUToolTestBase(unittest.TestCase):
     def setUp(self):
-        self.iwyu_args = []
         self.stdout_stub = StringIO()
         iwyu_tool.sys.stdout = self.stdout_stub
 
-    def _process_invocations(self,
-                             invocations,
-                             verbose=False,
-                             formatter=None,
-                             jobs=1):
+    def _execute(self, invocations, verbose=False, formatter=None, jobs=1):
         formatter = formatter or iwyu_tool.DEFAULT_FORMAT
         formatter = iwyu_tool.FORMATTERS.get(formatter, formatter)
-
         return iwyu_tool.execute(invocations, verbose, formatter, jobs)
 
 
 class IWYUToolTests(IWYUToolTestBase):
     def test_from_compile_command(self):
-        self.iwyu_args = ['-foo']
+        iwyu_args = ['-foo']
         invocation = iwyu_tool.Invocation.from_compile_command(
             {
                 'directory': '/home/user/llvm/build',
                 'command': '/usr/bin/clang++ -Iinclude file.cc',
                 'file': 'file.cc'
-            }, self.iwyu_args)
+            }, iwyu_args)
         self.assertEqual(
             invocation.command,
             [iwyu_tool.IWYU_EXECUTABLE, '-foo', '-Iinclude', 'file.cc'])
         self.assertEqual(invocation.cwd, '/home/user/llvm/build')
 
     def test_invocation(self):
-        invocation = MockInvocation([], '')
+        invocation = MockInvocation()
         invocation.will_return('BAR')
-        self.assertEqual(self._process_invocations([invocation]), 0)
+        self.assertEqual(self._execute([invocation]), 0)
         self.assertEqual(self.stdout_stub.getvalue(), 'BAR\n')
 
     def test_order_asynchronous(self):
-        invocations = [MockInvocation([], '') for _ in range(100)]
+        invocations = [MockInvocation() for _ in range(100)]
         for n, invocation in enumerate(invocations):
             invocation.will_return('BAR%d' % n)
             invocation.will_block(random.random() / 100)
-        self.assertEqual(self._process_invocations(invocations, jobs=100), 0)
+        self.assertEqual(self._execute(invocations, jobs=100), 0)
         self.assertSetEqual(
             set('BAR%d' % n for n in range(100)),
-            set(self.stdout_stub.getvalue()[:-1].splitlines()))
+            set(self.stdout_stub.getvalue().splitlines()))
 
     def test_order_synchronous(self):
-        invocations = [MockInvocation([], '') for _ in range(100)]
+        invocations = [MockInvocation() for _ in range(100)]
         for n, invocation in enumerate(invocations):
             invocation.will_return('BAR%d' % n)
             invocation.will_block(random.random() / 100)
-        self.assertEqual(self._process_invocations(invocations, jobs=1), 0)
+        self.assertEqual(self._execute(invocations, jobs=1), 0)
         self.assertEqual(['BAR%d' % n for n in range(100)],
-                         self.stdout_stub.getvalue()[:-1].splitlines())
+                         self.stdout_stub.getvalue().splitlines())
 
 
 if __name__ == '__main__':
