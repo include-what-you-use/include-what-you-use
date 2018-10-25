a9f3656c0d99557bc7f3cc2e829f5823dbc0c706
J.Ru <juanruben.segovia@gmail.com>
2018-10-25 08:48:20 +0200

    Require explicit inst. decls to use main template
    If the usage of a template has visible explicit instantiations decls,
    require them to be included along with the main template definition.
    
    ScanInstantiatedType has also been changed to make sure that the template
    being analyzed always corresponds to the definition to make it work as
    expected when there is an explicit instantiation definition visible.
    
    Fixes #558

---
+++ tests/cxx/explicit_instantiation2-template_short.h
@@ -0,0 +1,15 @@
+
+//===- explicit_instantiation2-template_short.h - test input file for iwyu ===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef INCLUDE_WHAT_YOU_USE_TESTS_CXX_EXPLICIT_INSTANTIATION2_TEMPLATE_SHORT_H_
+#define INCLUDE_WHAT_YOU_USE_TESTS_CXX_EXPLICIT_INSTANTIATION2_TEMPLATE_SHORT_H_
+
+extern template class Template<short>;
+
+#endif  // INCLUDE_WHAT_YOU_USE_TESTS_CXX_EXPLICIT_INSTANTIATION2_TEMPLATE_SHORT_H_
