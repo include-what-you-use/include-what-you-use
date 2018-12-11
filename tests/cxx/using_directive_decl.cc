97e236c55e5ebbd95b8bb221fd9497851f67c3cc
Tom Rix <tom.rix@sony.com>
2018-12-10 16:03:36 -0800

    Track use of namespace in using directive decl a.h: namespace a { ... };
    b.cpp:
    include "a.h"
    using namespace a;
    ...
    
    Add a test that header is not removed because of using directive
    
    Adjust badinc.cc test for new diagnostic output.

---
+++ tests/cxx/using_directive_decl.cc
@@ -0,0 +1,16 @@
+//===--- using_directive_decl.cc - test input file for iwyu ---------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#include "using_directive_decl_a.h"
+using namespace a;
+
+/**** IWYU_SUMMARY
+
+(tests/cxx/using_directive_decl.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
