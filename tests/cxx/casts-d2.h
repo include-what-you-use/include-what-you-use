aae9bdd85466ad85ddff87b29b58ecf024c32a38
Kim Grasman <kim.grasman@gmail.com>
2019-03-09 17:32:45 +0100

    Require converted-to type for upcasts via user-defined conversions
    When a derived-to-base cast succeeds because of a user-defined
    conversion, report the converted from-type as well.
    
    For example:
    
      struct X {
        operator Derived*() const { return nullptr; }
      };
    
      struct Base {};
      struct Derived : public Base {};
    
      X x;
      // Requires the full type for both X and Derived.
      static_cast<Base*>(x);
    
    We would previously forget to report Derived here, which fooled IWYU
    into making suggestions that would break working code.
    
    Fixes issue #650.

---
+++ tests/cxx/casts-d2.h
@@ -0,0 +1,10 @@
+//===--- casts-d2.h - test input file for iwyu ----------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tests/cxx/casts-i2.h"
--- tests/cxx/casts.cc
+++ tests/cxx/casts.cc
@@ -10,6 +10,8 @@
 // Tests that we handle correctly handle the various types of cast.
 
 #include "tests/cxx/casts-d1.h"
+#include "tests/cxx/casts-d2.h"
+#include "tests/cxx/casts-d3.h"
 
 // Prevent regression of a bug where we were ignoring the template
 // type arg when we should haven't been, when we were casting to it.
@@ -24,6 +26,14 @@ int main() {
   // IWYU: CastsSubclass needs a declaration
   // IWYU: CastsSubclass is...*casts-i1.h
   TestTemplateCastBug<CastsSubclass>(cc);
+
+  // IWYU: CastsI3Convertible is...*casts-i3.h
+  CastsI3Convertible ci3;
+
+  // IWYU: CastsI2Base needs a declaration
+  // IWYU: CastsI2Derived is...*casts-i2.h
+  // IWYU: CastsI3Convertible is...*casts-i3.h
+  (void)static_cast<CastsI2Base*>(ci3);
 }
 
 
@@ -31,11 +41,17 @@ int main() {
 
 tests/cxx/casts.cc should add these lines:
 #include "tests/cxx/casts-i1.h"
+#include "tests/cxx/casts-i2.h"
+#include "tests/cxx/casts-i3.h"
 
 tests/cxx/casts.cc should remove these lines:
 - #include "tests/cxx/casts-d1.h"  // lines XX-XX
+- #include "tests/cxx/casts-d2.h"  // lines XX-XX
+- #include "tests/cxx/casts-d3.h"  // lines XX-XX
 
 The full include-list for tests/cxx/casts.cc:
 #include "tests/cxx/casts-i1.h"  // for CastsClass (ptr only), CastsSubclass
+#include "tests/cxx/casts-i2.h"  // for CastsI2Base (ptr only), CastsI2Derived
+#include "tests/cxx/casts-i3.h"  // for CastsI3Convertible
 
 ***** IWYU_SUMMARY */
