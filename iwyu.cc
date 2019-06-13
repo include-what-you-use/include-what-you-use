f4a0390e729890b559b64428f6162ce73804a9f2
Zachary Henkel <zachary.henkel@gmail.com>
2019-06-13 11:53:55 -0500

    Don't crash if FileInfo cannot be found

--- iwyu.cc
+++ iwyu.cc
@@ -3708,7 +3708,9 @@ class IwyuAstConsumer
     // though, because that will drag in every overload even if we're
     // only using one.  Instead, we keep track of the using decl and
     // mark it as touched when something actually uses it.
-    preprocessor_info().FileInfoFor(CurrentFileEntry())->AddUsingDecl(decl);
+    auto file_info = preprocessor_info().FileInfoFor(CurrentFileEntry());
+    if (file_info)
+      fileInfo->AddUsingDecl(decl);
 
     if (CanIgnoreCurrentASTNode())  return true;
 
