adae66fd1d5a5fe6e0ed1bf427ce2f0b4ceeddba
Uladzislau Paulovich <ulad@fb.com>
2019-11-05 11:39:48 +0000

    Correctly handle enums which use typedefed types

---
+++ tests/cxx/enum_base-i1.h
@@ -0,0 +1,15 @@
+//===--- enum_base-i1.h - test input file for iwyu ------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef INCLUDE_WHAT_YOU_USE_TESTS_CXX_ENUM_BASE_I1_H_
+#define INCLUDE_WHAT_YOU_USE_TESTS_CXX_ENUM_BASE_I1_H_
+
+using int_t = int;
+
+#endif  // INCLUDE_WHAT_YOU_USE_TESTS_CXX_ENUM_BASE_I1_H_
