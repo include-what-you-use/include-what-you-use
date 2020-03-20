dc02499405e729f67e90077e497eceb101abb36e
John Bytheway <jbytheway@gmail.com>
2020-03-19 20:58:32 -0400

    Expand more templates during visitation
    In walking the AST there were various places where types were only
    expanded if !CanIgnoreType.  But the criteria were wrong
    
    The CanIgnoreType is suitable for determining whether a type was
    reportable, but not whether it was expansion-worthy.
    
    Add a new parameter to CanIgnoreType to distinguish the two use cases,
    and pass it in a couple of cases.
    
    Also, tweak the resugar map construction code.  Now it is capable of
    handling template aliases as well as template classes and functions.
    
    Add a new test that now passes with these changes.
    
    This also resolves a couple of TODOs in an existing test, so update that
    accordingly.

--- tests/cxx/alias_template.cc
+++ tests/cxx/alias_template.cc
@@ -11,19 +11,19 @@
 
 #include "tests/cxx/direct.h"
 
-template<class T> struct FullUseTemplateArg {
+template<class T> struct FullUseTemplateArgInSizeof {
   char argument[sizeof(T)];
 };
 
 // Test that we go through alias template and handle aliased template
 // specialization.
-template<class T> using Alias = FullUseTemplateArg<T>;
+template<class T> using Alias = FullUseTemplateArgInSizeof<T>;
 // IWYU: IndirectClass needs a declaration
 // IWYU: IndirectClass is...*indirect.h
 Alias<IndirectClass> alias;
 
 // Test following through entire chain of aliases.
-template<class T> using AliasChain1 = FullUseTemplateArg<T>;
+template<class T> using AliasChain1 = FullUseTemplateArgInSizeof<T>;
 template<class T> using AliasChain2 = AliasChain1<T>;
 // IWYU: IndirectClass needs a declaration
 // IWYU: IndirectClass is...*indirect.h
@@ -32,6 +32,18 @@ AliasChain2<IndirectClass> aliasChain;
 // Test the case when aliased type isn't a template specialization.
 template<class T> using Pointer = T*;
 Pointer<int> intPtr;
+
+template <class T>
+struct FullUseTemplateArgAsVar {
+  T t;
+};
+
+// Test the used class being nested deeper in the alias
+template <typename T>
+using AliasNested = FullUseTemplateArgAsVar<FullUseTemplateArgAsVar<T>>;
+// IWYU: IndirectClass needs a declaration
+// IWYU: IndirectClass is...*indirect.h
+AliasNested<IndirectClass> aliasNested;
 
 /**** IWYU_SUMMARY
 
