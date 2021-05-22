5b741d6eb0822a5eccf6a7130f3b27ff9f384337
Kim Grasman <kim.grasman@gmail.com>
2021-05-22 18:57:10 +0200

    Improve detection of builtin functions
    The technique used before relied on linear search of all builtins by
    name. This did not work with qualified C++ names of C library names such
    as std::pow and std::round.
    
    Use ASTContext to get to BuiltinInfo, and in turn use that to check more
    specific traits of the builtin by ID instead of name.
    
    Add distinct tests for C and C++ to cover both <math.h> and <cmath>.
    
    Fixes issue #776.

---
+++ tests/cxx/libbuiltins.cc
@@ -0,0 +1,34 @@
+//===--- libbuiltins.cc - test input file for iwyu ------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// Library builtins are, like normal builtins, compiled down to an intrinsic,
+// but a header still needs to be included for the program to be valid. The math
+// library (std::pow, std::round, etc) is a typical example.
+
+// IWYU_ARGS: -I .
+
+#include "tests/cxx/libbuiltins-direct.h"
+
+float kapow(float x) {
+  // IWYU: std::pow is...*cmath
+  return std::pow(x, 2.0F);
+}
+
+/**** IWYU_SUMMARY
+
+tests/cxx/libbuiltins.cc should add these lines:
+#include <cmath>
+
+tests/cxx/libbuiltins.cc should remove these lines:
+- #include "tests/cxx/libbuiltins-direct.h"  // lines XX-XX
+
+The full include-list for tests/cxx/libbuiltins.cc:
+#include <cmath>  // for pow
+
+***** IWYU_SUMMARY */
