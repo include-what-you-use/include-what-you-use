3dcdbf15206f2b134c69af38e7919e3d77c47b60
Alejandro Colomar <alx.manpages@gmail.com>
2021-07-04 14:25:16 +0200

    Add mappings for the macro 'MAP_STACK'
    Signed-off-by: Alejandro Colomar <alx.manpages@gmail.com>

--- iwyu_include_picker.cc
+++ iwyu_include_picker.cc
@@ -289,6 +289,8 @@ const IncludeMapEntry libc_symbol_map[] = {
   { "EOF", kPrivate, "<stdio.h>", kPublic },
   { "EOF", kPrivate, "<libio.h>", kPublic },
   { "FILE", kPrivate, "<stdio.h>", kPublic },
+  { "MAP_STACK", kPrivate, "<sys/mman.h>", kPublic },
+  { "MAP_STACK", kPrivate, "<linux/mman.h>", kPublic },
   { "va_list", kPrivate, "<stdarg.h>", kPublic },
   { "va_list", kPrivate, "<stdio.h>", kPublic },
   { "va_list", kPrivate, "<wchar.h>", kPublic },
