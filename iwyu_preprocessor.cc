058936b2614676719f4227e2f4cf6d100c2f07fe
Kim Gräsman <kim.grasman@gmail.com>
2022-02-20 13:50:37 +0100

    [clang compat] Use range-for over macro tokens
    Clang 0d9b91524ea4db3760791bba15773c386a26d8ec renamed the MacroInfo
    'tokens_iterator' to 'const_iterator', and broke our code. Use the more
    abstract range iteration over 'tokens' instead for nicer code and backwards
    compatibility.

--- iwyu_preprocessor.cc
+++ iwyu_preprocessor.cc
@@ -624,9 +624,7 @@ void IwyuPreprocessorInfo::MacroDefined(const Token& id,
   // #undefs and re-defines a macro, but should work fine in practice.)
   if (macro_loc.isValid())
     macros_definition_loc_[GetName(id)] = macro_loc;
-  for (MacroInfo::tokens_iterator it = macro->tokens_begin();
-       it != macro->tokens_end(); ++it) {
-    const Token& token_in_macro = *it;
+  for (const Token& token_in_macro : macro->tokens()) {
     if (token_in_macro.getKind() == clang::tok::identifier &&
         token_in_macro.getIdentifierInfo()->hasMacroDefinition()) {
       macros_called_from_macros_.push_back(token_in_macro);
