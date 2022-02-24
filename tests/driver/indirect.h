d912972891b1d0913f7b960eee73ec54f91aa371
Kim Gräsman <kim.grasman@gmail.com>
2022-02-24 21:38:32 +0100

    Add new test suite 'driver'
    For tests that don't really pertain to language, but have more to do
    with driver details, e.g. argument parsing.
    
    Add reusable indirect/direct headers up-front.

---
+++ tests/driver/indirect.h
@@ -0,0 +1,17 @@
+//===--- indirect.h - test input file for iwyu ----------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef INCLUDE_WHAT_YOU_USE_TESTS_DRIVER_INDIRECT_H_
+#define INCLUDE_WHAT_YOU_USE_TESTS_DRIVER_INDIRECT_H_
+
+struct Indirect {
+  int a;
+};
+
+#endif  // INCLUDE_WHAT_YOU_USE_TESTS_DRIVER_INDIRECT_H_
