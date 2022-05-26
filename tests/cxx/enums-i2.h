4c0f39615982c57a9cb313c971c6f45fca1cc1fe
Bolshakov <bolsh.andrey@yandex.ru>
2022-05-27 00:14:05 +0300

    Tests on enum opaque declarations

---
+++ tests/cxx/enums-i2.h
@@ -0,0 +1,15 @@
+//===--- enums-i2.h - test input file for iwyu ----------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef INCLUDE_WHAT_YOU_USE_TESTS_CXX_ENUMS_I2_H_
+#define INCLUDE_WHAT_YOU_USE_TESTS_CXX_ENUMS_I2_H_
+
+enum class IndirectEnum2 : int { A, B, C };
+
+#endif  // INCLUDE_WHAT_YOU_USE_TESTS_CXX_ENUMS_I2_H_
---
+++ tests/cxx/enums-i4.h
@@ -0,0 +1,15 @@
+//===--- enums-i4.h - test input file for iwyu ----------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef INCLUDE_WHAT_YOU_USE_TESTS_CXX_ENUMS_I4_H_
+#define INCLUDE_WHAT_YOU_USE_TESTS_CXX_ENUMS_I4_H_
+
+enum { UnnamedEnumItem1, UnnamedEnumItem2, UnnamedEnumItem3 };
+
+#endif  // INCLUDE_WHAT_YOU_USE_TESTS_CXX_ENUMS_I4_H_
