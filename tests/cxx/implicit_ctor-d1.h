d4fdd05cb8dbccd3ff7e5fbb3d9d70c8c27bfd65
Bolshakov <bolsh.andrey@yandex.ru>
2022-09-04 17:56:51 +0300

    Move autocast testing from badinc
    More cases are covered by iwyu_stricter_than_cpp.

--- tests/cxx/implicit_ctor-d1.h
+++ tests/cxx/implicit_ctor-d1.h
@@ -7,6 +7,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "tests/cxx/direct.h"
 #include "tests/cxx/implicit_ctor-i1.h"
 
 // We do need IndirectWithImplicitCtor even though we just take a
@@ -21,15 +22,35 @@ int ImplicitCtorFn(IndirectWithImplicitCtor);
 // IWYU: IndirectWithImplicitCtor is...*implicit_ctor-i2.h.*for autocast
 int ImplicitCtorRefFn(const IndirectWithImplicitCtor&);
 
+// Test parameter type uses that do not require special handling for "autocast".
+int NoAutocastFn(
+    // A subtle c++ point: forward-declaring is ok for nonconst, because
+    // you can't do implicit conversion to a non-const reference
+    // (implicit conversion involves creating a temporary, which
+    // doesn't bind to non-const references).
+    // IWYU: IndirectWithImplicitCtor needs a declaration
+    IndirectWithImplicitCtor& nonconst,
+    // Forward-declaring is ok because ptrref is a const reference
+    // to a *pointer*.
+    // IWYU: IndirectWithImplicitCtor needs a declaration
+    IndirectWithImplicitCtor* const& ptrref,
+    // IWYU: IndirectClass needs a declaration
+    const IndirectClass&,
+    // IWYU: IndirectClass needs a declaration
+    IndirectClass);
+
 /**** IWYU_SUMMARY
 
 tests/cxx/implicit_ctor-d1.h should add these lines:
 #include "tests/cxx/implicit_ctor-i2.h"
+class IndirectClass;
 
 tests/cxx/implicit_ctor-d1.h should remove these lines:
+- #include "tests/cxx/direct.h"  // lines XX-XX
 - #include "tests/cxx/implicit_ctor-i1.h"  // lines XX-XX
 
 The full include-list for tests/cxx/implicit_ctor-d1.h:
 #include "tests/cxx/implicit_ctor-i2.h"  // for IndirectWithImplicitCtor
+class IndirectClass;
 
 ***** IWYU_SUMMARY */
