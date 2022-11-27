d8d49b37adab4beda4de7fa5b4fe57ca372dbf98
Kim Gräsman <kim.grasman@gmail.com>
2022-11-27 13:23:42 +0100

    Avoid dynamic mapping to self for reverse macro dependency
    Commit 04f1c925370d0f1a70642147f07c70a4c2bfb530 added a mechanism that
    dynamically adds a mapping from the includee to the includer in this
    case:
    
        // a.h
        #if CONTROL_FLAG
        void foo();
        #else
        void bar();
        #endif
    
        // b.h
        #define CONTROL_FLAG
        #include "a.h"
    
    However, there was no check that includee and includer were different,
    so regular #include guards would cause self-mappings, which would
    eventually lead to cycles in mappings and assertion failures, e.g.:
    
        Cycle in include-mapping:
          <z.hpp> ->
          <z.hpp>
        ,,,/iwyu_include_picker.cc:845: Assertion failed: Cycle in include-mapping
        Aborted
    
    Avoid this by checking if the file defining the macro is the same as the
    file using it; in that case we don't need a mapping.

---
+++ tests/cxx/self_map_cycle-d1.h
@@ -0,0 +1,18 @@
+//===--- self_map_cycle-d1.h - test input file for iwyu -------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef INCLUDE_WHAT_YOU_USE_TESTS_CXX_SELF_MAP_CYCLE_D1_H_
+#define INCLUDE_WHAT_YOU_USE_TESTS_CXX_SELF_MAP_CYCLE_D1_H_
+
+#include "tests/cxx/self_map_cycle-d1.h"
+
+struct X {
+};
+
+#endif
