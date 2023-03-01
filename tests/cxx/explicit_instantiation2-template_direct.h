448eddf33ac120e8d943227c2497720d481f4c99
Kim Gräsman <kim.grasman@gmail.com>
2023-03-01 21:53:26 +0100

    Refactor explicit_instantiation2 testcase for extension
    I will be making some changes to the explicit instantiation policies,
    but this testcase has builtin assumptions that make that more difficult.
    
    More precisely, it doesn't make a difference between locally defined
    explicit instantiations vs. only header-defined explicit instantiations.
    
    Split those two kinds into Template<bool> (locally defined) and
    Template<short> (only from headers) to be able to make the distinction
    more clearly in future changes.
    
    No policy change.

---
+++ tests/cxx/explicit_instantiation2-template_direct.h
@@ -0,0 +1,16 @@
+//===--- explicit_instantiation2-template_direct.h - test input -----------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef INCLUDE_WHAT_YOU_USE_TESTS_CXX_EXPLICIT_INSTANTIATION2_TEMPLATE_DIRECT_H_
+#define INCLUDE_WHAT_YOU_USE_TESTS_CXX_EXPLICIT_INSTANTIATION2_TEMPLATE_DIRECT_H_
+
+#include "explicit_instantiation2-template_bool.h"
+#include "explicit_instantiation2-template_short.h"
+
+#endif  // INCLUDE_WHAT_YOU_USE_TESTS_CXX_EXPLICIT_INSTANTIATION2-TEMPLATE_DIRECT_H_
