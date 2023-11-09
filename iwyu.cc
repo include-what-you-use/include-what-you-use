include-what-you-use/include-what-you-use

commit c2df89d85103cbd04170c3c8004f648eefdad028

Analyze class template bases
Previously, class template bases were handled on definitions of template
specialization type variables due to the traversal of member initializer
list of (probably implicit) constructor. It didn't work for other
contexts, such as taking `sizeof` template specialization type.

iwyu.cc
@@ -120,6 +120,7 @@
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/Decl.h"
 #include "clang/AST/DeclBase.h"
+#include "clang/AST/DeclCXX.h"
 #include "clang/AST/DeclTemplate.h"
 #include "clang/AST/ExprConcepts.h"
 #include "clang/AST/NestedNameSpecifier.h"
@@ -148,6 +149,7 @@ using clang::ASTConsumer;
 using clang::ASTContext;
 using clang::ASTFrontendAction;
 using clang::Attr;
+using clang::CXXBaseSpecifier;
 using clang::CXXConstructExpr;
 using clang::CXXConstructorDecl;
 using clang::CXXDeleteExpr;
@@ -3485,6 +3487,11 @@ class InstantiatedTemplateVisitor
       if (isa<CXXMethodDecl>(*it) || isa<FunctionTemplateDecl>(*it))
         continue;
       if (!TraverseDecl(*it))
+        return false;
+    }
+
+    for (const CXXBaseSpecifier& base : class_decl->bases()) {
+      if (!TraverseType(base.getType()))
         return false;
     }
 

tests/cxx/template_base.cc
@@ -1,0 +1,62 @@
+//===--- template_base.cc - test input file for iwyu ----------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// IWYU_ARGS: -I .
+
+// Tests reporting types needed for class template base instantiation.
+
+#include "tests/cxx/direct.h"
+
+template <typename T>
+struct DirectInheritance : T {
+  struct Inner {};
+};
+
+template <typename T>
+struct InheritanceFromTplInheritedFromT : DirectInheritance<T> {};
+
+template <typename T>
+struct TplInBaseNNS : InheritanceFromTplInheritedFromT<T>::Inner {};
+
+// IWYU: IndirectClass needs a declaration
+// IWYU: IndirectClass is...*indirect.h
+constexpr auto s1 = sizeof(TplInBaseNNS<IndirectClass>);
+
+template <typename T>
+// TODO(bolshakov): forward-declaration is probably sufficient.
+// IWYU: IndirectTemplate is...*indirect.h
+struct InheritanceFromTplWithTMember : IndirectTemplate<T> {};
+
+// IWYU: IndirectClass needs a declaration
+// IWYU: IndirectClass is...*indirect.h
+constexpr auto s2 = sizeof(InheritanceFromTplWithTMember<IndirectClass>);
+
+template <typename T>
+struct PtrOnly {
+  T* t;
+};
+
+template <typename T>
+struct InheritanceFromPtrOnly : PtrOnly<T> {};
+
+// IWYU: IndirectClass needs a declaration
+constexpr auto s3 = sizeof(InheritanceFromPtrOnly<IndirectClass>);
+
+/**** IWYU_SUMMARY
+
+tests/cxx/template_base.cc should add these lines:
+#include "tests/cxx/indirect.h"
+
+tests/cxx/template_base.cc should remove these lines:
+- #include "tests/cxx/direct.h"  // lines XX-XX
+
+The full include-list for tests/cxx/template_base.cc:
+#include "tests/cxx/indirect.h"  // for IndirectClass, IndirectTemplate
+
+***** IWYU_SUMMARY */

commit df55a69e040987084fe72f0efcba068aa3e4fa51

Fix crash on scanning noninstantiated implicit spec
This fixes #1331.

The crash had been introduced in #1325, because there is an assert
in clang that the `bases()` method should be called only on class
declarations having definitions. But clang doesn't instantiate
definition of class template implicit specialization until that
specialization is fully used.

Maybe, the root of the problem is that IWYU tries to provide the full
type info when it is not actually needed, specifically, in default type
template arguments. Not sure what the correct behavior should be.
Currently, this goal isn't fully achieved because template internals
aren't scanned when the specialization is not instantiated. Even worse,
IWYU may give different suggestions to the header containing
the template definition depending on the translation unit as part
of which the header is analyzed.

iwyu.cc
@@ -3476,6 +3476,12 @@ class InstantiatedTemplateVisitor
     if (ReplayClassMemberUsesFromPrecomputedList(type))
       return true;
 
+    // Sometimes, an implicit specialization occurs to be not instantiated.
+    // TODO(bolshakov): don't report them at all as full uses or figure out
+    // how to scan them.
+    if (!class_decl->hasDefinition())
+      return true;
+
     // Make sure all the types we report in the recursive TraverseDecl
     // calls, below, end up in the cache for class_decl.
     CacheStoringScope css(&cache_storers_, ClassMembersFullUseCache(),

tests/cxx/default_tpl_arg-d1.h
@@ -1,0 +1,10 @@
+//===--- default_tpl_arg-d1.h - test input file for iwyu ------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tests/cxx/default_tpl_arg-i1.h"

tests/cxx/default_tpl_arg-i1.h
@@ -1,0 +1,13 @@
+//===--- default_tpl_arg-i1.h - test input file for iwyu ------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+template <typename T>
+struct UninstantiatedTpl {
+  T t;
+};

tests/cxx/default_tpl_arg.cc
@@ -1,0 +1,41 @@
+//===--- default_tpl_arg.cc - test input file for iwyu --------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// IWYU_ARGS: -I .
+
+// Tests handling default type template arguments. In particular, that IWYU
+// doesn't crash when they refer to uninstantiated template specializations.
+
+#include "tests/cxx/default_tpl_arg-d1.h"
+
+// IWYU: UninstantiatedTpl is...*default_tpl_arg-i1.h
+template <typename = UninstantiatedTpl<int>>
+struct Tpl {};
+
+template <typename T>
+struct Outer {
+  // IWYU: UninstantiatedTpl is...*default_tpl_arg-i1.h
+  template <typename = UninstantiatedTpl<T>>
+  struct Inner {};
+};
+
+Outer<int> o;
+
+/**** IWYU_SUMMARY
+
+tests/cxx/default_tpl_arg.cc should add these lines:
+#include "tests/cxx/default_tpl_arg-i1.h"
+
+tests/cxx/default_tpl_arg.cc should remove these lines:
+- #include "tests/cxx/default_tpl_arg-d1.h"  // lines XX-XX
+
+The full include-list for tests/cxx/default_tpl_arg.cc:
+#include "tests/cxx/default_tpl_arg-i1.h"  // for UninstantiatedTpl
+
+***** IWYU_SUMMARY */