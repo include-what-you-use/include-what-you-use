include-what-you-use/include-what-you-use

commit 69e61ca8ed06045bc40f7d909e4199a914dd58b2

Avoid explicit inst reports to template location
Clang attributes `ClassTemplateSpecializationDecl`s of explicit
instantiations to the instantiated template definition location. Hence
traversing them may cause reporting of template arguments incorrectly
to that location (which is usually in a header).

`TraverseClassTemplateSpecializationDecl` handler was added in #774
so as to analyze explicit specializations. It was seemingly an upstream
issue fixed in
https://github.com/llvm/llvm-project/commit/c119a17e7fd6fb528d8c1e1bbb0a
At now, default `TraverseClassTemplateSpecializationDecl` implementation
works well: traverses explicit specialization definition, but only
the name in case of an implicit specialization.

`TraverseCXXRecordDecl` is directly called
on `ClassTemplateSpecializationDecl` in `AnalyzeTemplateTypeParmUse`
to handle implicit specialization which may be needed on instantiation
of some other template (when the former doesn't occur to be written
as `TemplateSpecializationType` in the initial template arguments, or
in the template body). `alias_template.cc` test showed relating
regression.

`Inner` class template isn't very much needed for the test purpose
(a field of type `T` may be sufficient in `Template`), but it is added
to assure that the correct instantiated template visitation logic
is used.

iwyu.cc
@@ -578,15 +578,6 @@ class BaseAstVisitor : public RecursiveASTVisitor<Derived> {
     return true;
   }
 
-  // Class template specialization are similar to regular C++ classes,
-  // particularly they need the same custom handling of implicit destructors.
-  bool TraverseClassTemplateSpecializationDecl(
-      clang::ClassTemplateSpecializationDecl* decl) {
-    if (!Base::TraverseClassTemplateSpecializationDecl(decl))
-      return false;
-    return Base::TraverseCXXRecordDecl(decl);
-  }
-
   //------------------------------------------------------------
   // (5) Add TraverseImplicitDestructorCall and HandleFunctionCall.
 
@@ -3150,7 +3141,8 @@ class InstantiatedTemplateVisitor
         VERRS(6) << "Recursively traversing " << PrintableDecl(cts_decl)
                  << " which was full-used and does not involve a known"
                  << " template param\n";
-        TraverseDecl(const_cast<ClassTemplateSpecializationDecl*>(cts_decl));
+        TraverseCXXRecordDecl(
+            const_cast<ClassTemplateSpecializationDecl*>(cts_decl));
       }
     }
   }

tests/cxx/expl_inst_select.cc
@@ -19,7 +19,6 @@
 template class Template<char>;
 
 // An explicit instantiation declaration for later use.
-// IWYU: Template needs a declaration...*
 // IWYU: Template is...*expl_inst_select-i1.h
 extern template class Template<int>;
 

tests/cxx/explicit_instantiation-template.h
@@ -9,6 +9,27 @@
 #ifndef INCLUDE_WHAT_YOU_USE_TESTS_CXX_EXPLICIT_INSTANTIATION_TEMPLATE_H_
 #define INCLUDE_WHAT_YOU_USE_TESTS_CXX_EXPLICIT_INSTANTIATION_TEMPLATE_H_
 
-template<typename T> class Template {};
+#include "tests/cxx/direct.h"
+
+template <typename T>
+class Inner {
+  T t;
+};
+
+template <typename T>
+class Template {
+  Inner<T> x;
+};
 
 #endif  // INCLUDE_WHAT_YOU_USE_TESTS_CXX_EXPLICIT_INSTANTIATION_TEMPLATE_H_
+
+/**** IWYU_SUMMARY
+
+tests/cxx/explicit_instantiation-template.h should add these lines:
+
+tests/cxx/explicit_instantiation-template.h should remove these lines:
+- #include "tests/cxx/direct.h"  // lines XX-XX
+
+The full include-list for tests/cxx/explicit_instantiation-template.h:
+
+***** IWYU_SUMMARY */

tests/cxx/explicit_instantiation.cc
@@ -7,7 +7,9 @@
 //
 //===----------------------------------------------------------------------===//
 
-// IWYU_ARGS: -Xiwyu --check_also="tests/cxx/explicit_instantiation-spec.h" -I .
+// IWYU_ARGS: -Xiwyu --check_also=tests/cxx/explicit_instantiation-spec.h \
+//            -Xiwyu --check_also=tests/cxx/explicit_instantiation-template.h \
+//            -I .
 
 #include "tests/cxx/explicit_instantiation-spec.h"
 #include "tests/cxx/explicit_instantiation-template_direct.h"
@@ -40,10 +42,20 @@ extern template class Template<char>;
 extern template class Template<int*>;
 template class Template<int*>;
 
+// IWYU: Template is...*explicit_instantiation-template.h
+// IWYU: IndirectClass needs a declaration
+// IWYU: IndirectClass is...*indirect.h
+extern template class Template<IndirectClass>;
+// IWYU: Template is...*explicit_instantiation-template.h
+// IWYU: IndirectClass needs a declaration
+// IWYU: IndirectClass is...*indirect.h
+template class Template<IndirectClass>;
+
 /**** IWYU_SUMMARY
 
 tests/cxx/explicit_instantiation.cc should add these lines:
 #include "tests/cxx/explicit_instantiation-template.h"
+#include "tests/cxx/indirect.h"
 
 tests/cxx/explicit_instantiation.cc should remove these lines:
 - #include "tests/cxx/explicit_instantiation-template_direct.h"  // lines XX-XX
@@ -51,5 +63,6 @@ tests/cxx/explicit_instantiation.cc should remove these lines:
 The full include-list for tests/cxx/explicit_instantiation.cc:
 #include "tests/cxx/explicit_instantiation-spec.h"  // for Template
 #include "tests/cxx/explicit_instantiation-template.h"  // for Template
+#include "tests/cxx/indirect.h"  // for IndirectClass
 
 ***** IWYU_SUMMARY */

commit aed53ea8c569b199e3e33248fb1e79be85e998b8

Add testcase for issue #1336
This bug report came in roughly in parallel with PR #1332, which happens
to fix it.

The anonymous type aspect of the bug never made it into the test suite,
so add it as an isolated test case here.

Already passes, but fails with 69e61ca8ed06045bc40f7d909e4199a914dd58b2
reverted.

tests/cxx/expl_spec_anonymous.cc
@@ -1,0 +1,59 @@
+//===--- expl_spec_anonymous.cc - test input file for iwyu ----------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// https://github.com/include-what-you-use/include-what-you-use/issues/1336
+// describes how IWYU would suggest removing anonymous types nested inside class
+// template specializations as if they were unused forward-decls:
+//
+//   tests/cxx/expl_spec_anonymous.cc should add these lines:
+//
+//   tests/cxx/expl_spec_anonymous.cc should remove these lines:
+//   -   // lines 33-33
+//   -   // lines 37-37
+//   -   // lines 43-43
+//   -   // lines 47-47
+//
+//   The full include-list for tests/cxx/expl_spec_anonymous.cc:
+//   ---
+//
+// This was fixed in 69e61ca8ed06045bc40f7d909e4199a914dd58b2, but the isolated
+// repro case never made it into the tree.
+
+// A base template.
+template <class T>
+class Class {};
+
+// A specialization containing anonymous types.
+template <>
+class Class<int> {
+  union {
+    float a;
+    int b;
+  };
+  struct {
+    float c;
+    int d;
+  };
+
+  // Bonus: unnamed types declaring named members.
+  union {
+    float a;
+    int b;
+  } x;
+  struct {
+    float a;
+    int b;
+  } y;
+};
+
+/**** IWYU_SUMMARY
+
+(tests/cxx/expl_spec_anonymous.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */