include-what-you-use/include-what-you-use

commit 2d26dd1b33d53c4347010aaf6544238dcbefe36a

Improve 'SubstTemplateTypeParmType' determination
`getAs` doesn't check intermediate types in the sugar chain, only
the type itself and its canonical type.

iwyu_ast_util.cc
@@ -563,6 +563,17 @@ static const Type* GetTemplateArgAsType(const TemplateArgument& tpl_arg) {
   return nullptr;
 }
 
+static bool IsSubstTemplateTypeParmType(const Type* type) {
+  const Type* prev_type = type;
+  do {
+    if (isa<SubstTemplateTypeParmType>(type))
+      return true;
+    prev_type = type;
+    type = type->getLocallyUnqualifiedSingleStepDesugaredType().getTypePtr();
+  } while (type != prev_type);
+  return false;
+}
+
 // These utilities figure out the template arguments and other type components
 // that are specified in various contexts: TemplateSpecializationType (for
 // template classes), FunctionDecl (for template functions), and type aliases.
@@ -667,7 +678,7 @@ class TypeEnumeratorWithoutSubstituted
   bool TraverseTypeHelper(QualType qual_type) {
     CHECK_(!qual_type.isNull());
     const Type* type = qual_type.getTypePtr();
-    if (type->getAs<SubstTemplateTypeParmType>())
+    if (IsSubstTemplateTypeParmType(type))
       return true;
 
     seen_types_.insert(GetCanonicalType(type));

tests/cxx/typedef_in_template.cc
@@ -122,6 +122,9 @@ struct Identity {
   struct Inner {
     using Type = T;
   };
+
+  static T t;
+  using SugaredType = decltype(t);
 };
 
 template <typename T>
@@ -168,6 +171,10 @@ void ArgumentTypeProvision() {
   // IWYU: IndirectClass needs a declaration
   Outer<IndirectClass*>::Inner<Providing>::AliasedTpl ptr_p;
 }
+
+// IWYU: IndirectClass needs a declaration
+// IWYU: IndirectClass is...*indirect.h
+constexpr auto s3 = sizeof(Identity<IndirectClass>::SugaredType);
 
 /**** IWYU_SUMMARY
 

commit 827a69558298f5b7d10230ed20d347b9e6875e0e

Fix detection of substituted tpl param in alias
It turns out that the presence of `SubstTemplateTypeParmType` inside
an alias type sugar chain doesn't necessarily mean that it is
a user-provided template parameter. It may occur e. g. when an alias
template is used inside the `typedef` declaration, and its template
parameter is specified inside that declaration. The regression had been
introduced in 2d26dd1b33 and found on MS STL `vector` implementation
when IWYU started to require `_Simple_types` type info for such a code:

std::vector v{1, 2, 3};
(void)v.begin();

due to the returned `iterator` type alias.

iwyu_ast_util.cc
@@ -568,6 +568,11 @@ static bool IsSubstTemplateTypeParmType(const Type* type) {
   do {
     if (isa<SubstTemplateTypeParmType>(type))
       return true;
+    if (isa<TypedefType>(type) || isa<TemplateSpecializationType>(type)) {
+      // Still may be substituted outer template parameter but needs additional
+      // analysis outside this function.
+      return false;
+    }
     prev_type = type;
     type = type->getLocallyUnqualifiedSingleStepDesugaredType().getTypePtr();
   } while (type != prev_type);

tests/cxx/typedef_chain_no_follow-d4.h
@@ -1,0 +1,15 @@
+//===--- typedef_chain_no_follow-d4.h - test input file for iwyu ----------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tests/cxx/typedef_chain_class.h"
+
+template <typename T>
+using IdentityAlias = T;
+
+using ProvidingWithAliasTpl = IdentityAlias<TypedefChainClass>;

tests/cxx/typedef_chain_no_follow.cc
@@ -15,6 +15,7 @@
 #include "tests/cxx/typedef_chain_no_follow-d1.h"
 #include "tests/cxx/typedef_chain_no_follow-d2.h"
 #include "tests/cxx/typedef_chain_no_follow-d3.h"
+#include "tests/cxx/typedef_chain_no_follow-d4.h"
 // Unused include to trigger IWYU summary telling what symbols are used from
 // every file.
 #include "tests/cxx/direct.h"
@@ -22,6 +23,9 @@
 void TypedefDeclaredInGlobalNamespace() {
   TypedefChainTypedef tct;
   tct.Method();
+
+  ProvidingWithAliasTpl pwat;
+  pwat.Method();
 }
 
 // Tests how we handle a typedef declared in a class.  Main purpose is to make
@@ -50,5 +54,6 @@ The full include-list for tests/cxx/typedef_chain_no_follow.cc:
 #include "tests/cxx/typedef_chain_no_follow-d1.h"  // for TypedefChainTypedef
 #include "tests/cxx/typedef_chain_no_follow-d2.h"  // for NonContainer1
 #include "tests/cxx/typedef_chain_no_follow-d3.h"  // for NonContainer2
+#include "tests/cxx/typedef_chain_no_follow-d4.h"  // for ProvidingWithAliasTpl
 
 ***** IWYU_SUMMARY */