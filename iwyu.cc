include-what-you-use/include-what-you-use

commit f0aede4441bae186b3cc3f998d693bb1d4564f88

Avoid redundant reports on fn decl references
TraverseDeclRefExpr should work only with implicitly instantiated
function template specialization definition references. Other types
of function references don't require full return type info because they
don't cause template instantiation.

iwyu.cc
@@ -178,6 +178,7 @@ using clang::FriendTemplateDecl;
 using clang::FunctionDecl;
 using clang::FunctionProtoType;
 using clang::FunctionTemplateDecl;
+using clang::FunctionTemplateSpecializationInfo;
 using clang::FunctionType;
 using clang::LValueReferenceType;
 using clang::LinkageSpecDecl;
@@ -794,6 +795,13 @@ class BaseAstVisitor : public RecursiveASTVisitor<Derived> {
       return true;
 
     if (FunctionDecl* fn_decl = DynCastFrom(expr->getDecl())) {
+      const FunctionTemplateSpecializationInfo* tpl_spec_info =
+          fn_decl->getTemplateSpecializationInfo();
+      const bool is_def = fn_decl->isThisDeclarationADefinition();
+      if (!tpl_spec_info ||  // I. e. not a template specialization, or...
+          tpl_spec_info->isExplicitInstantiationOrSpecialization() || !is_def) {
+        return true;
+      }
       // If fn_decl has a class-name before it -- 'MyClass::method' --
       // it's a method pointer.
       const Type* parent_type = nullptr;

tests/cxx/funcptrs-i1.h
@@ -44,10 +44,20 @@ Enum Function(Class*) {
   return E_one;
 }
 
+// Forward declaration means that FunctionReturningRecordType doesn't provide
+// its return type.
+class IndirectClass;
+IndirectClass FunctionReturningRecordType();
+
 template<typename T>
 int FunctionTemplate(Class*) {
   return T(10).Value();
 }
+
+template <typename T>
+IndirectClass FunctionTemplate2();
+
+extern template IndirectClass FunctionTemplate2<int>();
 
 template<typename T>
 class ClassTemplate {

tests/cxx/funcptrs.cc
@@ -19,6 +19,7 @@
 // A 'function' can be a free function, a static member function, a member
 // function, or any template instantiation of the above.
 
+#include "tests/cxx/direct.h"
 #include "tests/cxx/funcptrs-d1.h"
 
 
@@ -79,6 +80,17 @@ void FreeFunctions() {
   // IWYU: Retval is...*funcptrs-i1.h
   // IWYU: FunctionTemplate is...*funcptrs-i1.h
   &FunctionTemplate<Retval>;
+
+  // Full return type info is needed neither for non-templated function...
+  // IWYU: FunctionReturningRecordType is...*funcptrs-i1.h
+  &FunctionReturningRecordType;
+  // ... nor for function template implicit instantiation without definition...
+  // IWYU: FunctionTemplate2 is...*funcptrs-i1.h
+  &FunctionTemplate2<char>;
+  // ... nor for function template explicit specialization or instantiation
+  // references.
+  // IWYU: FunctionTemplate2 is...*funcptrs-i1.h
+  &FunctionTemplate2<int>;
 }
 
 void ClassMembers() {
@@ -212,9 +224,10 @@ tests/cxx/funcptrs.cc should add these lines:
 #include "tests/cxx/funcptrs-i1.h"
 
 tests/cxx/funcptrs.cc should remove these lines:
+- #include "tests/cxx/direct.h"  // lines XX-XX
 - #include "tests/cxx/funcptrs-d1.h"  // lines XX-XX
 
 The full include-list for tests/cxx/funcptrs.cc:
-#include "tests/cxx/funcptrs-i1.h"  // for Class, ClassTemplate, Enum, Function, FunctionTemplate, Retval
+#include "tests/cxx/funcptrs-i1.h"  // for Class, ClassTemplate, Enum, Function, FunctionReturningRecordType, FunctionTemplate, FunctionTemplate2, Retval
 
 ***** IWYU_SUMMARY */

commit 8acf3f1711f69d3a09becc89fd38cce788159a6a

Fix handling function references
Additional filtering of function references has been removed. It is not
needed now because reporting of return type is avoided.

This fixes reporting of template argument type when the template has no
definition in the translation unit but the full type info is needed
to build the instantiated function prototype, as well as handling
instantiations of regular member functions of class templates. It was
a regression introduced in #1300.

`nodes_to_ignore_` restored after function traversal so as to avoid
short-circuiting inside `IwyuBaseAstVisitor::HandleFunctionCall` which
could occur because function declaration was already traversed on its
`DeclRefExpr` handling.

iwyu.cc
@@ -830,8 +830,6 @@ class BaseAstVisitor : public RecursiveASTVisitor<Derived> {
       return true;
 
     if (FunctionDecl* fn_decl = DynCastFrom(expr->getDecl())) {
-      if (!IsImplicitlyInstantiatedDfn(fn_decl))
-        return true;
       if (const auto* call_expr =  // Skip intermediate ImplicitCastExpr node.
           current_ast_node_->GetAncestorAs<CallExpr>(2)) {
         if (call_expr->getDirectCallee() == fn_decl)
@@ -3453,6 +3451,7 @@ class InstantiatedTemplateVisitor
     // the uninstantiated function, so we don't need to re-traverse
     // them here.
     AstFlattenerVisitor nodeset_getter(compiler());
+    ValueSaver<AstFlattenerVisitor::NodeSet> s(&nodes_to_ignore_);
     // This gets to the decl for the (uninstantiated) template-as-written:
     const FunctionDecl* decl_as_written =
         fn_decl->getTemplateInstantiationPattern();

tests/cxx/funcptrs-i1.h
@@ -57,6 +57,9 @@ int FunctionTemplate(Class*) {
 template <typename T>
 IndirectClass FunctionTemplate2();
 
+template <typename T>
+decltype(T::StaticMemberFunction()) FunctionTemplateWithoutDef();
+
 extern template IndirectClass FunctionTemplate2<int>();
 
 template<typename T>
@@ -64,11 +67,19 @@ class ClassTemplate {
  public:
   int MemberFunction() const { return 20; }
 
+  void MemberFunctionUsingTplArg() {
+    T t;
+  }
+
   template<typename R>
   int MemberTemplate() const { return R(50).Value(); }
 
   static int StaticMemberFunction() {
     return 100;
+  }
+
+  static void StaticMemberFunctionUsingTplArg() {
+    T t;
   }
 
   template<typename R>

tests/cxx/funcptrs.cc
@@ -91,6 +91,11 @@ void NonMemberFunctions() {
   // references.
   // IWYU: FunctionTemplate2 is...*funcptrs-i1.h
   &FunctionTemplate2<int>;
+
+  // IWYU: FunctionTemplateWithoutDef is...*funcptrs-i1.h
+  // IWYU: Class needs a declaration
+  // IWYU: Class is...*funcptrs-i1.h
+  &FunctionTemplateWithoutDef<Class>;
 }
 
 void ClassMembers() {
@@ -201,6 +206,11 @@ void ClassTemplateMembers() {
   // IWYU: Class needs a declaration
   &ClassTemplate<Class>::StaticMemberFunction;
 
+  // IWYU: ClassTemplate is...*funcptrs-i1.h
+  // IWYU: Class needs a declaration
+  // IWYU: Class is...*funcptrs-i1.h
+  &ClassTemplate<Class>::StaticMemberFunctionUsingTplArg;
+
   // IWYU: Retval needs a declaration
   // IWYU: Retval is...*funcptrs-i1.h
   // IWYU: ClassTemplate is...*funcptrs-i1.h
@@ -210,6 +220,11 @@ void ClassTemplateMembers() {
   // IWYU: ClassTemplate is...*funcptrs-i1.h
   // IWYU: Class needs a declaration
   &ClassTemplate<Class>::MemberFunction;
+
+  // IWYU: ClassTemplate is...*funcptrs-i1.h
+  // IWYU: Class needs a declaration
+  // IWYU: Class is...*funcptrs-i1.h
+  &ClassTemplate<Class>::MemberFunctionUsingTplArg;
 
   // IWYU: Retval needs a declaration
   // IWYU: Retval is...*funcptrs-i1.h
@@ -228,6 +243,6 @@ tests/cxx/funcptrs.cc should remove these lines:
 - #include "tests/cxx/funcptrs-d1.h"  // lines XX-XX
 
 The full include-list for tests/cxx/funcptrs.cc:
-#include "tests/cxx/funcptrs-i1.h"  // for Class, ClassTemplate, Enum, Function, FunctionReturningRecordType, FunctionTemplate, FunctionTemplate2, Retval
+#include "tests/cxx/funcptrs-i1.h"  // for Class, ClassTemplate, Enum, Function, FunctionReturningRecordType, FunctionTemplate, FunctionTemplate2, FunctionTemplateWithoutDef, Retval
 
 ***** IWYU_SUMMARY */