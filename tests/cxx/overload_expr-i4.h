a82c71187d3e3a8a0d0caac1034138a02090669d
Bolshakov <bolsh.andrey@yandex.ru>
2024-07-02 15:39:16 +0300

    Report unresolved refs to variable templates
    The change in badinc.cc is because I1_NamespaceTemplateFn shadow decl
    is now reported on CallOverloadWithUsingShadowDecl scanning, hence
    using-declaration is considered as being used and may not require
    `#include` by itself (as for the other using-decls). If its reference
    is removed from CallOverloadWithUsingShadowDecl, the suggestion comes
    back, as it should be.
    
    This fixes #792, #966, #1335, #1516, and #1558.

--- tests/cxx/overload_expr-i4.h
+++ tests/cxx/overload_expr-i4.h
@@ -7,7 +7,16 @@
 //
 //===----------------------------------------------------------------------===//
 
+#ifndef INCLUDE_WHAT_YOU_USE_TESTS_CXX_OVERLOAD_EXPR_I4_H_
+#define INCLUDE_WHAT_YOU_USE_TESTS_CXX_OVERLOAD_EXPR_I4_H_
+
 // All the OverloadedFn2 overloads visible without ADL are in the same file.
 
 void OverloadedFn2(int);
 void OverloadedFn2(char);
+
+// Variable template.
+template <typename>
+int var_tpl;
+
+#endif  // INCLUDE_WHAT_YOU_USE_TESTS_CXX_OVERLOAD_EXPR_I4_H_
