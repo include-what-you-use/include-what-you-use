cb326f9bfb59b0281dcbcd09b61ac81140024d9b
Bolshakov <bolsh.andrey@yandex.ru>
2024-11-25 15:35:10 +0300

    Don't report type sugar from implicit code
    Skip any sugar type nodes in any implicitly generated code, because type
    sugar is used only when it is explicitly written.
    
    The additional methods have been placed into IwyuAstConsumer and not
    into InstantiatedTemplateVisitor because the latter should not report
    type sugar in any case (it should not even have any methods doing this).
    
    The change in VisitType has been made because an exact type from
    an exception specification (e.g. ElaboratedType) may be skipped now.
    
    This fixes #1613.

---
+++ tests/cxx/no_implicit_sugar_report_cxx11-direct.h
@@ -0,0 +1,12 @@
+//===--- no_implicit_sugar_report_cxx11-direct.h - test input -------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tests/cxx/no_implicit_sugar_report_cxx11-indirect.h"
+
+TypedefHost::Int GetInt();
---
+++ tests/cxx/no_implicit_sugar_report_cxx11-indirect.h
@@ -0,0 +1,12 @@
+//===--- no_implicit_sugar_report_cxx11-indirect.h - test input -----------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+struct TypedefHost {
+  typedef int Int;
+};
