529fe4a762b8e2f7e48e595e858b73eea512358c
Kim Gräsman <kim.grasman@gmail.com>
2025-01-01 21:58:41 +0100

    Sort includes in iwyu_driver.cc
    VirtualFileSystem.h was added in e2e42c6b0ec2777d, but for some reason I missed
    alpha-sorting it.
    
    No functional change.

--- iwyu_driver.cc
+++ iwyu_driver.cc
@@ -47,9 +47,9 @@
 #include "llvm/Option/Option.h"
 #include "llvm/Support/ErrorOr.h"
 #include "llvm/Support/FileSystem.h"
-#include "llvm/Support/VirtualFileSystem.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/Path.h"
+#include "llvm/Support/VirtualFileSystem.h"
 #include "llvm/TargetParser/Host.h"
 
 // TODO: Clean out pragmas as IWYU improves.
