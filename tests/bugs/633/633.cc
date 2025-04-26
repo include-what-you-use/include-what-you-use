bd56cb910182e7c2f5756ffccfd65756e338bb33
Kim Gräsman <kim.grasman@gmail.com>
2025-04-26 16:58:30 +0200

    633

---
+++ tests/bugs/633/633.cc
@@ -0,0 +1,13 @@
+// Shows that IWYU crashes with --check_also="*"; there's something about
+// matching all possible files that's tripping up our include-name handling.
+
+// IWYU_ARGS: -Xiwyu --check_also="*"
+// IWYU_XFAIL
+
+void nothing();
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/633/633.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
