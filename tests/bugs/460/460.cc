0c3e10bd78fe4c09e6e0df8db9e030961f925aea
Kim Gräsman <kim.grasman@gmail.com>
2025-04-26 14:01:13 +0200

    460

---
+++ tests/bugs/460/460.cc
@@ -0,0 +1,24 @@
+// IWYU_XFAIL
+
+#include "foo.h"
+
+int main() {
+  bar::MyVec c;
+  bar::MyVec::iterator cit = c.begin();
+  while (cit != c.end()) {
+    cit++;
+  }
+
+  bar2::MyList d;
+  bar2::MyList::iterator dit = d.begin();
+  while (dit != d.end()) {
+    dit++;
+  }
+  return 0;
+}
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/460/460.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
