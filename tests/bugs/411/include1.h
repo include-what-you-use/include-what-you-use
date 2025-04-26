38b21150ce413eefb917cac03d4b7e1474f6f17c
Kim Gräsman <kim.grasman@gmail.com>
2025-04-26 13:26:43 +0200

    411

---
+++ tests/bugs/411/include1.h
@@ -0,0 +1,17 @@
+#ifndef __INCLUDE1_H__
+
+#ifndef __disabled_things
+#define __INCLUDE1_H__ 1
+
+struct importantthing {
+  int x, y, z;
+};
+#define OPTION1 2
+#define OPTION2 4
+
+#endif
+
+extern int superimportantvariable;  // always visible
+
+#undef __disabled_things
+#endif  //__INCLUDE1_H__
