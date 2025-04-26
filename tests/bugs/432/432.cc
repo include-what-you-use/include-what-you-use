7af6046da66e67570e3eb9bbf46d971ed906fbc1
Kim Gräsman <kim.grasman@gmail.com>
2025-04-26 13:37:29 +0200

    432

---
+++ tests/bugs/432/432.cc
@@ -0,0 +1,15 @@
+// IWYU_XFAIL
+
+#include <stri\
+ng>
+
+/**** IWYU_SUMMARY
+
+tests/bugs/432/432.cc should add these lines:
+
+tests/bugs/432/432.cc should remove these lines:
+- #include <string>  // lines XX-XX
+
+The full include-list for tests/bugs/432/432.cc:
+
+***** IWYU_SUMMARY */
