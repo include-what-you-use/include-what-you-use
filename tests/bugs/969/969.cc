92c70cbd06704efa71ad13ef61e9722ff493003d
Kim Gräsman <kim.grasman@gmail.com>
2025-04-27 16:56:31 +0200

    969

---
+++ tests/bugs/969/969.cc
@@ -0,0 +1,15 @@
+// IWYU_XFAIL
+
+#define define_my_var           \
+struct my_struct {              \
+        int my_member;          \
+};                              \
+struct my_struct my_var;
+
+define_my_var
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/969/969.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
