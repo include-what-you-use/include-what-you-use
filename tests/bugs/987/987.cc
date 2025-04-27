d96d0d9a5d96a59eca7452c0985bbdbf28608300
Kim Gräsman <kim.grasman@gmail.com>
2025-04-27 17:29:31 +0200

    987

---
+++ tests/bugs/987/987.cc
@@ -0,0 +1,19 @@
+// IWYU_XFAIL
+#include "987.h"
+
+// This is already included in 987.h, and so could be removed here.
+#include <cstdint>
+
+void foo(std::uint8_t arg) {
+}
+
+/**** IWYU_SUMMARY
+
+tests/bugs/987/987.cc should add these lines:
+
+tests/bugs/987/987.cc should remove these lines:
+- #include <cstdint>  // lines XX-XX
+
+The full include-list for tests/bugs/987/987.cc:
+
+***** IWYU_SUMMARY */
