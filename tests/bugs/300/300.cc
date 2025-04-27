b8033de1918e4a997b218e0570fa496d640fac63
Kim Gräsman <kim.grasman@gmail.com>
2025-04-27 11:29:57 +0200

    300

---
+++ tests/bugs/300/300.cc
@@ -0,0 +1,17 @@
+// IWYU_ARGS: -stdlib=libc++
+// IWYU_XFAIL
+
+#include <string>
+#include <unordered_set>
+
+static const std::unordered_set<std::string> us;
+
+bool f() {
+  return (us.find("") != us.end());
+}
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/300/300.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
