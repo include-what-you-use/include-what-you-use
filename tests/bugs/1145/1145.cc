3fc6ccae9700390fb6516afdce79c212a803e58f
Kim Gräsman <kim.grasman@gmail.com>
2025-04-27 19:24:32 +0200

    1145

---
+++ tests/bugs/1145/1145.cc
@@ -0,0 +1,25 @@
+// IWYU_XFAIL
+
+#include "subclass.h"
+
+template <typename T>
+struct TObjectPtr2 {
+ public:
+  template <typename U>
+  TObjectPtr2(U&& Object) : ObjectPtr(Object) {
+  }
+  T* ObjectPtr;
+};
+
+struct TestClass {
+ public:
+  TObjectPtr2<BaseClass> Foo;
+  TestClass(SubClass* InFoo) : Foo(InFoo) {
+  }
+};
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/1145/1145.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
---
+++ tests/bugs/1145/subclass.h
@@ -0,0 +1,2 @@
+#include "baseclass.h"
+class SubClass : public BaseClass {};
