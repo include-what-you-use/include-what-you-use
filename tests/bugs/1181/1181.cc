18f90575b0229c1f69d39dbbfd8505cb0b7882aa
Kim Gräsman <kim.grasman@gmail.com>
2025-04-28 20:36:24 +0200

    1181

---
+++ tests/bugs/1181/1181.cc
@@ -0,0 +1,11 @@
+// IWYU_XFAIL
+
+#include "1181.h"
+enum class color { RED = 2 };
+enum_data<color> d;
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/1181/1181.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
