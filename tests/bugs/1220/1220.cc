1de3be51e41765001a98e9bee593b4f1766ab92d
Kim Gräsman <kim.grasman@gmail.com>
2025-04-28 20:48:03 +0200

    1220

---
+++ tests/bugs/1220/1220.cc
@@ -0,0 +1,13 @@
+// IWYU_ARGS: -Xiwyu -v3
+// IWYU_XFAIL
+
+#include <array> // IWYU pragma: keep
+// IWYU pragma: no_include <cstddef>
+
+std::size_t x = 123;
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/1220/1220.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
