e672c34fdc81210a296782a50ef6347efb3dc32a
Kim Gräsman <kim.grasman@gmail.com>
2025-05-01 17:30:44 +0200

    1626

---
+++ tests/bugs/1626/1626.cc
@@ -0,0 +1,34 @@
+// IWYU_ARGS: -std=c++17
+// IWYU_XFAIL
+
+#include "unary_expression.h"
+#include "expression.h"
+
+// IWYU will suggest forward-declaring UnaryExpression, which causes this file
+// to no longer compile.
+
+template <typename T>
+struct ExpressionVisitor {
+  virtual ~ExpressionVisitor() = default;
+  virtual bool visitExpression(typename T::Expression& e);
+};
+
+template <typename T>
+bool ExpressionVisitor<T>::visitExpression(typename T::Expression& e) {
+  if (e.template is<UnaryExpression>()) {
+    auto& b = e.template as<UnaryExpression>();
+    return bool(b.expr());
+  }
+  return false;
+}
+
+struct ExpressionVisitorTypes {
+  using Expression = const Expression;
+};
+template struct ExpressionVisitor<ExpressionVisitorTypes>;
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/1626/1626.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
---
+++ tests/bugs/1626/expression.h
@@ -0,0 +1,25 @@
+#ifndef EXPRESSION
+#define EXPRESSION
+
+enum class ExpressionKind : int {
+    kUnary = 0,
+    kLiteral,
+};
+
+class Expression {
+public:
+    Expression(ExpressionKind kind) : fKind(kind) {}
+
+    template <typename T> bool is() const {
+        return fKind == T::kIRNodeKind;
+    }
+
+    template <typename T> const T& as() const {
+        return static_cast<const T&>(*this);
+    }
+
+private:
+    ExpressionKind fKind;
+};
+
+#endif
