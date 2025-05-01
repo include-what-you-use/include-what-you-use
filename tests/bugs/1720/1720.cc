fc3af3f652d1758f9ee5838fa934c42a8435386f
Kim Gräsman <kim.grasman@gmail.com>
2025-05-01 18:30:15 +0200

    1720

---
+++ tests/bugs/1720/1720.cc
@@ -0,0 +1,16 @@
+// IWYU_ARGS: -I tests/bugs/1720 \
+//            -include public.h \
+//            -Xiwyu --mapping_file=tests/bugs/1720/m.imp \
+//            -Xiwyu --prefix_header_includes=remove
+// IWYU_XFAIL
+
+#include "another.h"
+
+s32 val;
+ComplexType other;
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/1720/1720.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
