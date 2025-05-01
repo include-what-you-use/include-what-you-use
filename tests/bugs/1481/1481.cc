7d92f142308ccd2ff60cda367be387b805dd38e5
Kim Gräsman <kim.grasman@gmail.com>
2025-05-01 15:02:38 +0200

    1481

---
+++ tests/bugs/1481/1481.cc
@@ -0,0 +1,18 @@
+// IWYU_XFAIL
+#include <cstdlib>
+
+// Jury's still out on whether sys/wait.h should be required here.
+// <cstdlib> is documented to provide WIFEXITED.
+#include <sys/wait.h>
+
+void f(int res) {
+  if (WIFEXITED(res)) {
+    abort();
+  }
+}
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/1481/1481.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
