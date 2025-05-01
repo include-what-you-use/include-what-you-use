f479581080275ddf5b8d766cb99499f0a9481c59
Kim Gräsman <kim.grasman@gmail.com>
2025-05-01 16:01:44 +0200

    1569

---
+++ tests/bugs/1569/1569.cc
@@ -0,0 +1,10 @@
+// IWYU_XFAIL
+
+// The export pragma should protect this #include.
+#include "f1.h"    // IWYU pragma: export
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/1569/1569.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
---
+++ tests/bugs/1569/f1.h
@@ -0,0 +1,6 @@
+#ifndef F1_H
+#define F1_H
+
+// empty
+
+#endif // F1_H
