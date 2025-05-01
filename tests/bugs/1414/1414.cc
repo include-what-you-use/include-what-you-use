2889c4ecb9834d01f2e870e613a538119bfcdb25
Kim Gräsman <kim.grasman@gmail.com>
2025-05-01 14:26:52 +0200

    1414

---
+++ tests/bugs/1414/1414.cc
@@ -0,0 +1,22 @@
+// IWYU_ARGS: -std=c++20
+// IWYU_XFAIL
+
+#include <concepts>
+#include "derived.h"
+
+class Base;
+
+template <typename T, typename U>
+  requires std::derived_from<T, U>
+class C {};
+
+int main() {
+  C<Derived, Base>();
+  return 0;
+}
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/1414/1414.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
---
+++ tests/bugs/1414/derived.h
@@ -0,0 +1,8 @@
+#ifndef DERIVED_H
+#define DERIVED_H
+
+#include "base.h"
+
+class Derived : public Base {};
+
+#endif
