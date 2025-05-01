ba76ca77247ee3145291f39ffabfa88ec253ac80
Kim Gräsman <kim.grasman@gmail.com>
2025-05-01 17:48:31 +0200

    1651

---
+++ tests/bugs/1651/1651.cc
@@ -0,0 +1,23 @@
+// IWYU_ARGS: -stdlib=libstdc++
+// IWYU_XFAIL
+#include "1651.h"
+
+#include <iostream>
+#include <string>
+
+void f(const S& s) {
+  std::cout << s.str;
+}
+
+/**** IWYU_SUMMARY
+
+tests/bugs/1651/1651.cc should add these lines:
+
+tests/bugs/1651/1651.cc should remove these lines:
+- #include <string>  // lines XX-XX
+
+The full include-list for tests/bugs/1651/1651.cc:
+#include "1651.h"
+#include <iostream>  // for basic_ostream, operator<<, cout
+
+***** IWYU_SUMMARY */
---
+++ tests/bugs/1651/1651.h
@@ -0,0 +1,11 @@
+#include <string>
+
+struct S {
+  std::string str;
+};
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/1651/1651.h has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
