11e147a03d968caa0824f0b06850334aa377db61
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:18 +0200

    1707 license

--- tests/bugs/1707/1707.cc
+++ tests/bugs/1707/1707.cc
@@ -1,3 +1,12 @@
+//===--- 1707.cc - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_XFAIL
 #include "1707.h"
 
--- tests/bugs/1707/1707.h
+++ tests/bugs/1707/1707.h
@@ -1,3 +1,12 @@
+//===--- 1707.h - iwyu test -----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #pragma once
 
 #include <type_traits>
