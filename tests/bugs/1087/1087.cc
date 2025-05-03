d3b788d0137714bf95a83ef6dd9c265b79f5fb61
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 12:59:36 +0200

    1087 license

--- tests/bugs/1087/1087.cc
+++ tests/bugs/1087/1087.cc
@@ -1,3 +1,12 @@
+//===--- 1087.cc - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_ARGS: -I tests/bugs/1087 -include b.h
 // IWYU_XFAIL
 
