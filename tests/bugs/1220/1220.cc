921de95beb7b82655ef760f31f998ab57aedf405
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:02:57 +0200

    1220 license

--- tests/bugs/1220/1220.cc
+++ tests/bugs/1220/1220.cc
@@ -1,3 +1,12 @@
+//===--- 1220.cc - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_ARGS: -Xiwyu -v3
 // IWYU_XFAIL
 
