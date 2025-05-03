374d63b56860b8d821991b0ea87cc45e9dc86e42
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:03:31 +0200

    1349 license

--- tests/bugs/1349/vector.h
+++ tests/bugs/1349/vector.h
@@ -1,3 +1,12 @@
+//===--- vector.h - iwyu test ---------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #include <vector>
 
 template <typename T>
