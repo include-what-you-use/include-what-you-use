477837cad4cf9bcbaea2701e9c2c4fb83bee398d
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:19 +0200

    752 license

--- tests/bugs/752/752.cc
+++ tests/bugs/752/752.cc
@@ -1,3 +1,12 @@
+//===--- 752.cc - iwyu test -----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_XFAIL
 // IWYU_ARGS: -std=c++2a
 
--- tests/bugs/752/compat.h
+++ tests/bugs/752/compat.h
@@ -1,3 +1,12 @@
+//===--- compat.h - iwyu test ---------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #pragma once
 
 #include <optional>
