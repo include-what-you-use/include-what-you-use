36a50946ec07920428114eb190acabee857b819d
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:03:55 +0200

    1428 license

--- tests/bugs/1428/1428.cc
+++ tests/bugs/1428/1428.cc
@@ -1,3 +1,12 @@
+//===--- 1428.cc - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_ARGS: -Xiwyu --mapping_file=tests/bugs/1428/m.imp
 // IWYU_XFAIL
 
