4886a9f607592b499cbc4370fc2011a9f4739fdc
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:03:40 +0200

    1370 license

--- tests/bugs/1370/b.h
+++ tests/bugs/1370/b.h
@@ -1,3 +1,12 @@
+//===--- b.h - iwyu test --------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef B_H
 #define B_H
 
