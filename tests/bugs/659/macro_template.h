030614103a641a2b308e36c9a5f4f9be21656bc1
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:19 +0200

    659 license

--- tests/bugs/659/macro_template.h
+++ tests/bugs/659/macro_template.h
@@ -1,3 +1,12 @@
+//===--- macro_template.h - iwyu test -------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #define MACRO() MacroClass()
 
 class MacroClass {
