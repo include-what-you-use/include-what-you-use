8631b0bbda3769835b16588404b16c71c8c1e158
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:04:21 +0200

    1546 license

--- tests/bugs/1546/macro.h
+++ tests/bugs/1546/macro.h
@@ -1,3 +1,12 @@
+//===--- macro.h - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #pragma once
 #include <string>
 
