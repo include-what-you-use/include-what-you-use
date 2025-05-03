44c600004041d1d6d82d16881202f46aa3a237ce
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:19 +0200

    917 license

--- tests/bugs/917/917.cc
+++ tests/bugs/917/917.cc
@@ -1,3 +1,12 @@
+//===--- 917.cc - iwyu test -----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_ARGS: -std=c++20
 // IWYU_XFAIL
 
