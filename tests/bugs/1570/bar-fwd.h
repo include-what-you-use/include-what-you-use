67d2b78aa6068e1416069e33ad2d59485bf67235
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:18 +0200

    1570 license

--- tests/bugs/1570/bar-fwd.h
+++ tests/bugs/1570/bar-fwd.h
@@ -1,3 +1,12 @@
+//===--- bar-fwd.h - iwyu test --------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef BAR_FWD_H
 #define BAR_FWD_H
 
--- tests/bugs/1570/foo-fwd.h
+++ tests/bugs/1570/foo-fwd.h
@@ -1,3 +1,12 @@
+//===--- foo-fwd.h - iwyu test --------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef FWD_FOO_H
 #define FWD_FOO_H
 
