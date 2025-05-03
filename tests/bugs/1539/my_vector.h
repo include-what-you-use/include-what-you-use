973acec3bbe80451777e0d968de66b3361354f57
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:04:09 +0200

    1539 license

--- tests/bugs/1539/my_vector.h
+++ tests/bugs/1539/my_vector.h
@@ -1,3 +1,12 @@
+//===--- my_vector.h - iwyu test ------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #pragma once
 
 #include <vector>
