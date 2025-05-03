c50b239b34f5ff936dcdf9bba4dc794b9172e58f
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:19 +0200

    965 license

--- tests/bugs/965/965.h
+++ tests/bugs/965/965.h
@@ -1,3 +1,12 @@
+//===--- 965.h - iwyu test ------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #include "a.h"
 
 template <typename T>
