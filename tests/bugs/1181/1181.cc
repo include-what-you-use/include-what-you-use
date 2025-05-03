86c250c92d575a2401d5e6cc80520030de6d537f
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:02:47 +0200

    1181 license

--- tests/bugs/1181/1181.cc
+++ tests/bugs/1181/1181.cc
@@ -1,3 +1,12 @@
+//===--- 1181.cc - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_XFAIL
 
 #include "1181.h"
