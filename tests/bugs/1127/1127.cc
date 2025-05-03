76df787782c611e53cf0e2098a75256582588b2d
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:01:56 +0200

    1127 license

--- tests/bugs/1127/1127.cc
+++ tests/bugs/1127/1127.cc
@@ -1,3 +1,12 @@
+//===--- 1127.cc - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_XFAIL
 
 #include "c.h"
