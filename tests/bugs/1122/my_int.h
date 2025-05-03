25c8fc06d25cc0fd1bf5af25fbe54aab5df0f744
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 12:59:49 +0200

    1122 license

--- tests/bugs/1122/my_int.h
+++ tests/bugs/1122/my_int.h
@@ -1,3 +1,12 @@
+//===--- my_int.h - iwyu test ---------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef MY_INT_H
 #define MY_INT_H
 
--- tests/bugs/1122/operations.h
+++ tests/bugs/1122/operations.h
@@ -1,3 +1,12 @@
+//===--- operations.h - iwyu test -----------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef OPERATIONS_H
 #define OPERATIONS_H
 
