af3ac753486089536c11cc8f6260c3ae964f9998
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:19 +0200

    598 license

--- tests/bugs/598/598.cc
+++ tests/bugs/598/598.cc
@@ -1,3 +1,12 @@
+//===--- 598.cc - iwyu test -----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_XFAIL
 
 #include "container.h"
