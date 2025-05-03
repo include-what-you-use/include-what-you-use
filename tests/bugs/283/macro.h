2eafefbe7a01b435b03729d8c9291ccbb764dde1
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:18 +0200

    283 license

--- tests/bugs/283/macro.h
+++ tests/bugs/283/macro.h
@@ -1,3 +1,12 @@
+//===--- macro.h - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // macro.h
 #define MACRO_A(domain) \
   void myfunc() { \
