3bdd73356b76a544c2b244bf4fcd2bd8df5e286f
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:18 +0200

    1645 license

--- tests/bugs/1645/bar.h
+++ tests/bugs/1645/bar.h
@@ -1,3 +1,12 @@
+//===--- bar.h - iwyu test ------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #pragma once
 #include "foo.h"
 
