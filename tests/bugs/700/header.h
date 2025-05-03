2369bc27b90430b74f15571177a1c424259561d1
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:19 +0200

    700 license

--- tests/bugs/700/header.h
+++ tests/bugs/700/header.h
@@ -1,3 +1,12 @@
+//===--- header.h - iwyu test ---------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 namespace FooNs {
   class FooClass {};
 };
