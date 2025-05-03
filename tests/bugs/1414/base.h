5802b7d696e370512c1aa1e66518a87a27502222
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:03:47 +0200

    1414 license

--- tests/bugs/1414/base.h
+++ tests/bugs/1414/base.h
@@ -1,3 +1,12 @@
+//===--- base.h - iwyu test -----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef BASE_H
 #define BASE_H
 
