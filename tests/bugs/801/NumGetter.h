3842c5741f2ede91fcfb866355b6e6a471ea687b
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:19 +0200

    801 license

--- tests/bugs/801/NumGetter.h
+++ tests/bugs/801/NumGetter.h
@@ -1,3 +1,12 @@
+//===--- NumGetter.h - iwyu test ------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 template <typename T>
 struct IntegerWrapper {};
 
