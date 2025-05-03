1c21b36392972627147da032633f86c77a87cf0c
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:19 +0200

    411 license

--- tests/bugs/411/include2.h
+++ tests/bugs/411/include2.h
@@ -1,3 +1,12 @@
+//===--- include2.h - iwyu test -------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef __INCLUDE2_H__
 #define __INCLUDE2_H__
 
