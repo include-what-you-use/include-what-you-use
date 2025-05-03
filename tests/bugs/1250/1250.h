152117c34e7289d4e4a30a57f4f7a72144546364
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:03:07 +0200

    1250 license

--- tests/bugs/1250/1250.h
+++ tests/bugs/1250/1250.h
@@ -1,3 +1,12 @@
+//===--- 1250.h - iwyu test -----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #pragma once
 
 #include "bar.h"
