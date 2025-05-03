c842b6874198877f7fca87d0226d779e1fef1bf0
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 12:59:58 +0200

    1122b license

--- tests/bugs/1122b/table.h
+++ tests/bugs/1122b/table.h
@@ -1,3 +1,12 @@
+//===--- table.h - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #pragma once
 
 template <typename T>
