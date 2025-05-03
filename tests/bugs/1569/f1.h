bbf2fb86be65eae37aa0d6e59736d8fba114dbb1
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:18 +0200

    1569 license

--- tests/bugs/1569/f1.h
+++ tests/bugs/1569/f1.h
@@ -1,3 +1,12 @@
+//===--- f1.h - iwyu test -------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef F1_H
 #define F1_H
 
