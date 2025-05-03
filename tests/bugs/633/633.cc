d676ff69e91bc76146044f10a14fd7ab1d5a31e8
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:19 +0200

    633 license

--- tests/bugs/633/633.cc
+++ tests/bugs/633/633.cc
@@ -1,3 +1,12 @@
+//===--- 633.cc - iwyu test -----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // Shows that IWYU crashes with --check_also="*"; there's something about
 // matching all possible files that's tripping up our include-name handling.
 
