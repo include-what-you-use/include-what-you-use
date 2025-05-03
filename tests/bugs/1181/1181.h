94ffb7d785c678c0581298ae94da06323152cb01
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:02:47 +0200

    1181 license

--- tests/bugs/1181/1181.h
+++ tests/bugs/1181/1181.h
@@ -1,3 +1,12 @@
+//===--- 1181.h - iwyu test -----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 template <typename> struct enum_data {
   static const int staticVar;
   virtual void var() const;
