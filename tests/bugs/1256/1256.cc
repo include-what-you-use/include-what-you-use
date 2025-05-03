de42bff8c40a0cf580c849b7024c980b7c0d3b38
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:03:13 +0200

    1256 license

--- tests/bugs/1256/1256.cc
+++ tests/bugs/1256/1256.cc
@@ -1,3 +1,12 @@
+//===--- 1256.cc - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_XFAIL
 
 #include "1256.h"
