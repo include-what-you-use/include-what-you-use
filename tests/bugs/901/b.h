a937e88c855a096a8be9c52407cf89a245fec574
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:19 +0200

    901 license

--- tests/bugs/901/b.h
+++ tests/bugs/901/b.h
@@ -1,3 +1,12 @@
+//===--- b.h - iwyu test --------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #include "a.h"
 
 struct MyData {
