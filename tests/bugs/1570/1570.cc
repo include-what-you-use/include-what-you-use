730468ec70719c47d6a15d1dfafc9124d7ee1dff
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:18 +0200

    1570 license

--- tests/bugs/1570/1570.cc
+++ tests/bugs/1570/1570.cc
@@ -1,3 +1,12 @@
+//===--- 1570.cc - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_XFAIL
 
 // IWYU does not understand forwarding headers.
