575d8d4770b2e28ac9d04cd1c050af3d3d6d6fd2
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:19 +0200

    300 license

--- tests/bugs/300/300.cc
+++ tests/bugs/300/300.cc
@@ -1,3 +1,12 @@
+//===--- 300.cc - iwyu test -----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_ARGS: -stdlib=libc++
 // IWYU_XFAIL
 
