c5a06b1f3883ebcaf9d657d5aaa438bd13bf162c
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:18 +0200

    1569 license

--- tests/bugs/1569/1569.cc
+++ tests/bugs/1569/1569.cc
@@ -1,3 +1,12 @@
+//===--- 1569.cc - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_XFAIL
 
 // The export pragma should protect this #include.
