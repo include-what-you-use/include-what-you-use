e7783e17db71e764f6ddca39a6da41296055600a
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:04:15 +0200

    1541 license

--- tests/bugs/1541/1541.cc
+++ tests/bugs/1541/1541.cc
@@ -1,3 +1,12 @@
+//===--- 1541.cc - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 // IWYU_XFAIL
 
 #include <list>
