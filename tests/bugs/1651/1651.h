89b6619b4adb71c358f970762837be53672ff922
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:18 +0200

    1651 license

--- tests/bugs/1651/1651.h
+++ tests/bugs/1651/1651.h
@@ -1,3 +1,12 @@
+//===--- 1651.h - iwyu test -----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #include <string>
 
 struct S {
