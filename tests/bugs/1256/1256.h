b6966b432ef0d44e2b35acaf8b5a15e56cc724be
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:03:13 +0200

    1256 license

--- tests/bugs/1256/1256.h
+++ tests/bugs/1256/1256.h
@@ -1,3 +1,12 @@
+//===--- 1256.h - iwyu test -----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 struct results;
 
 struct funcs {
