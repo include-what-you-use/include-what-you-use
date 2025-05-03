5c82364fa9d394372d2a1fafe67d73ca387428c4
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:18 +0200

    297 license

--- tests/bugs/297/t.h
+++ tests/bugs/297/t.h
@@ -1,3 +1,12 @@
+//===--- t.h - iwyu test --------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #include <memory>
 
 template <typename T>
