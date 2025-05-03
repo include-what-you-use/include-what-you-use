ddd603f64c6ac3226ccbb4408681dcf045afc50e
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:19 +0200

    801 license

--- tests/bugs/801/IntegerWrapperFoo.h
+++ tests/bugs/801/IntegerWrapperFoo.h
@@ -1,3 +1,12 @@
+//===--- IntegerWrapperFoo.h - iwyu test ----------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 template <>
 struct IntegerWrapper<Foo> {
   static constexpr int num = Foo::num;
