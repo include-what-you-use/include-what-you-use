631cd6197458be41f565416003a71a7d4e620642
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:18 +0200

    1665 license

--- tests/bugs/1665/inner.h
+++ tests/bugs/1665/inner.h
@@ -1,3 +1,12 @@
+//===--- inner.h - iwyu test ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 typedef struct Inner
 {
     int w = 0;
