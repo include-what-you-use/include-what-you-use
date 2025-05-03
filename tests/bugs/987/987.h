97ad2734a76db9c3c81b6d2ea7ac0b79f8afbbe8
Kim Gräsman <kim.grasman@gmail.com>
2025-05-03 13:07:19 +0200

    987 license

--- tests/bugs/987/987.h
+++ tests/bugs/987/987.h
@@ -1,3 +1,12 @@
+//===--- 987.h - iwyu test ------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #include <cstdint>
 
 void foo(std::uint8_t arg);
