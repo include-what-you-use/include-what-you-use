7bdc7298814cab563f0fea3d14e63ee9d5855e59
Kim Gräsman <kim.grasman@gmail.com>
2025-07-22 19:01:14 +0200

    Add bug test for issue #1759

---
+++ tests/bugs/1759/bar.h
@@ -0,0 +1,12 @@
+//===--- bar.h - iwyu test ------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "foo.h"
+
+Foo getFoo();
