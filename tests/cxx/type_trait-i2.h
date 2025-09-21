65aefad72cf70e47ddaac3bedfba6f30d26ef274
Bolshakov <bolsh.andrey@yandex.ru>
2025-09-21 22:33:58 +0300

    Handle new *synthesises_from_spaceship type traits
    These were added in
    https://github.com/llvm/llvm-project/commit/b1a8089f1cf67a6038a6b6d958eff1bd2feb4f8d.
    
    The operator lookup logic has mostly been copy-pasted from
    EvaluateBinaryTypeTrait function in clang/lib/Sema/SemaTypeTraits.cpp.
    
    This fixes #1814.

--- tests/cxx/type_trait-i2.h
+++ tests/cxx/type_trait-i2.h
@@ -14,4 +14,12 @@
 
 class Derived : public Base {};
 
+struct With3WayComp {
+  int operator<=>(int) const;
+  int operator<=>(int**) const;
+  int operator<=>(const Struct&) const;
+  int operator<=>(const Base*) const;
+  int operator<=>(const Union1*) const;
+};
+
 #endif  // INCLUDE_WHAT_YOU_USE_TESTS_CXX_TYPE_TRAIT_I2_H_
