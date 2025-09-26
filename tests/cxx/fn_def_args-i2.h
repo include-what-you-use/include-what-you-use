70d1305727e71c576ca9bd63904f080f1833dc4b
Bolshakov <bolsh.andrey@yandex.ru>
2025-09-26 19:07:31 +0300

    Select fn redecl with def arg used at call site
    The duplicating reporting of function declaration from VisitDeclRefExpr
    has been avoided because DeclRefExpr may point not to the correct
    redeclaration. However, if the function name is introduced by a using-
    declaration, that using-decl is still to be reported.
    
    The change in IsDefArgSpecified is to avoid an assertion failure
    on a templated constructor of std::set reported from
    HandleAliasedClassMethods. In principle, the default argument handling
    is not needed for class methods.

--- tests/cxx/fn_def_args-i2.h
+++ tests/cxx/fn_def_args-i2.h
@@ -7,6 +7,19 @@
 //
 //===----------------------------------------------------------------------===//
 
+#ifndef INCLUDE_WHAT_YOU_USE_TESTS_CXX_FN_DEF_ARGS_I2_H_
+#define INCLUDE_WHAT_YOU_USE_TESTS_CXX_FN_DEF_ARGS_I2_H_
+
+#include <cstddef>
+
 int GetInt();
 
 void FnWithSmearedDefArgs(int, int = 0);
+void FnWithSmearedDefArgs2(int, int = 0);
+void FnWithSmearedDefArgs3(int, int, int);
+void FnWithSmearedDefArgs3(int, int, int = 0);
+void FnWithDefArg(int = 0);
+
+void* operator new(std::size_t, int, int, int = 0);
+
+#endif  // INCLUDE_WHAT_YOU_USE_TESTS_CXX_FN_DEF_ARGS_I2_H_
--- tests/cxx/fn_def_args.cc
+++ tests/cxx/fn_def_args.cc
@@ -14,6 +14,10 @@
 
 #include "tests/cxx/fn_def_args-d1.h"
 
+// IWYU: FnWithSmearedDefArgs3 is...*-i1.h
+void FnWithSmearedDefArgs3(int = 0, int, int);
+void FnWithDefArg(int);
+
 template <typename T>
 void TplFn() {
   T::FnWithDefArg();
@@ -22,10 +26,52 @@ void TplFn() {
 void Fn() {
   TplFn<Struct>();
   Struct::FnWithDefArg();
+
+  // IWYU: FnWithSmearedDefArgs is...*-i2.h
+  FnWithSmearedDefArgs(1);
+  FnWithSmearedDefArgs();
+
+  // IWYU: FnWithSmearedDefArgs2 is...*-i2.h
+  FnWithSmearedDefArgs2(1);
+  // IWYU: FnWithSmearedDefArgs2 is...*-i1.h
+  FnWithSmearedDefArgs2();
+
+  // IWYU: FnWithSmearedDefArgs2 is...*-i2.h
+  // IWYU: ns::FnWithSmearedDefArgs2 is...*-i3.h.*for using decl
+  ns::FnWithSmearedDefArgs2(1);
+  // IWYU: FnWithSmearedDefArgs2 is...*-i1.h
+  // IWYU: ns::FnWithSmearedDefArgs2 is...*-i3.h.*for using decl
+  ns::FnWithSmearedDefArgs2();
+
+  // An appropriate redeclaration is present in this file.
+  FnWithSmearedDefArgs3();
+  FnWithSmearedDefArgs3(1);
+  FnWithSmearedDefArgs3(1, 1);
+  FnWithSmearedDefArgs3(1, 1, 1);
+
+  // IWYU: FnWithDefArg is...*-i2.h
+  FnWithDefArg();
+  // An appropriate redeclaration is present in this file.
+  FnWithDefArg(1);
+
+  // IWYU: operator new is...*-i2.h
+  new (1, 1) int;
+  new (1) int;
 }
 
 /**** IWYU_SUMMARY
 
-(tests/cxx/fn_def_args.cc has correct #includes/fwd-decls)
+tests/cxx/fn_def_args.cc should add these lines:
+#include "tests/cxx/fn_def_args-i1.h"
+#include "tests/cxx/fn_def_args-i2.h"
+#include "tests/cxx/fn_def_args-i3.h"
+
+tests/cxx/fn_def_args.cc should remove these lines:
+
+The full include-list for tests/cxx/fn_def_args.cc:
+#include "tests/cxx/fn_def_args-d1.h"  // for FnWithSmearedDefArgs, Struct, operator new
+#include "tests/cxx/fn_def_args-i1.h"  // for FnWithSmearedDefArgs2, FnWithSmearedDefArgs3
+#include "tests/cxx/fn_def_args-i2.h"  // for FnWithDefArg, FnWithSmearedDefArgs, FnWithSmearedDefArgs2, operator new
+#include "tests/cxx/fn_def_args-i3.h"  // for FnWithSmearedDefArgs2
 
 ***** IWYU_SUMMARY */
