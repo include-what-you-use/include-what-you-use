9232a10a4acd775ec0a7c74ca440f346abfccdde
Bolshakov <bolsh.andrey@yandex.ru>
2025-11-19 22:31:54 +0300

    Add C++ standard library symbol mapping
    The mapping has been generated from C++23 final draft (N4950) LaTeX
    sources downloaded from https://github.com/cplusplus/draft repository.
    
    Headers described in [input.output] clause are skipped because
    declarations from <iosfwd> cannot at now be marked as forward-
    declarations in the mapping. By the same reason, some other classes
    are explicitly excluded.
    
    Functions overloaded in different headers, as well as operators, are
    currently excluded from the mapping so as not to cause any regression
    because IWYU cannot currently distinguish them in mappings. Probably,
    they could be included when the mapping is complete. In that case, IWYU
    would at least not to recommend to remove a required header if it is
    already included in a user's source code.
    
    Preprocessor macro names have been excluded by a somewhat similar
    reason: so that IWYU does not suggest to replace C headers defining them
    with C++ ones.
    
    Formally, this fixes #1616, #1763, and probably #1863. However, similar
    bugs may appear until all the mapping has been written.

---
+++ tests/cxx/std_symbol_mapping-direct.h
@@ -0,0 +1,13 @@
+//===--- std_symbol_mapping-direct.h - test input file for iwyu -----------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+namespace std {
+template <class, class>
+struct pair;
+}
