include-what-you-use/include-what-you-use

commit 87ee0ee8151bdf623fd3e22fe0a9c3a403fe7a1a

Support variable template specs in mappings
The change in GetInstantiatedFromDecl and in GetDefinitionAsWritten is
required because otherwise, for partially specialized variable
templates, an implicitly instantiated specialization of the partial
specialization comes into GetWrittenQualifiedNameAsString instead of
the partial specialization itself.

iwyu_ast_util.cc
@@ -11,7 +11,6 @@
 
 #include "iwyu_ast_util.h"
 
-#include <cstddef>
 #include <regex>
 #include <set>                          // for set
 #include <string>                       // for string, operator+, etc
@@ -56,6 +55,7 @@
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/PointerUnion.h"
 #include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/StringExtras.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/Casting.h"
 
@@ -166,8 +166,11 @@ using clang::UnresolvedLookupExpr;
 using clang::UsingDirectiveDecl;
 using clang::ValueDecl;
 using clang::VarDecl;
+using clang::VarTemplateDecl;
+using clang::VarTemplatePartialSpecializationDecl;
 using clang::VarTemplateSpecializationDecl;
 using llvm::ArrayRef;
+using llvm::ListSeparator;
 using llvm::PointerUnion;
 using llvm::cast;
 using llvm::dyn_cast;
@@ -175,6 +178,7 @@ using llvm::dyn_cast_or_null;
 using llvm::errs;
 using llvm::isa;
 using llvm::range_size;
+using llvm::raw_ostream;
 using llvm::raw_string_ostream;
 using llvm::zip_equal;
 using std::function;
@@ -187,7 +191,7 @@ namespace include_what_you_use {
 
 namespace {
 
-void DumpASTNode(llvm::raw_ostream& ostream, const ASTNode* node) {
+void DumpASTNode(raw_ostream& ostream, const ASTNode* node) {
   if (const Decl *decl = node->GetAs<Decl>()) {
     ostream << "[" << GetKindName(decl) << "] " << PrintableDecl(decl);
   } else if (const Stmt *stmt = node->GetAs<Stmt>()) {
@@ -248,6 +252,36 @@ static const RedeclarableTemplateDecl* GetRedeclNotSpecifyingAnyDefArg(
       return redecl;
   }
   return nullptr;
+}
+
+template <typename T>
+static void PrintExplicitSpecTplArgs(const T* spec,
+                                     const PrintingPolicy& printing_policy,
+                                     raw_ostream& ostream) {
+  if (spec->isExplicitSpecialization()) {
+    ostream << '<';
+    const TemplateArgumentList& args = spec->getTemplateArgs();
+    const TemplateParameterList* params =
+        spec->getSpecializedTemplate()->getTemplateParameters();
+    ListSeparator LS;  // ', ' by default.
+    for (unsigned i = 0, ie = args.size(); i < ie; ++i) {
+      bool include_type = TemplateParameterList::shouldIncludeTypeForArgument(
+          printing_policy, params, i);
+      const TemplateArgument& arg = args.get(i);
+      // Clang encloses arguments corresponding to a parameter pack in angle
+      // brackets, so cannot use the default 'print' for them.
+      if (arg.getKind() == TemplateArgument::Pack) {
+        for (TemplateArgument pack_arg : arg.getPackAsArray()) {
+          ostream << LS;
+          pack_arg.print(printing_policy, ostream, include_type);
+        }
+      } else {
+        ostream << LS;
+        args.get(i).print(printing_policy, ostream, include_type);
+      }
+    }
+    ostream << '>';
+  }
 }
 
 }  // anonymous namespace
@@ -538,40 +572,18 @@ string GetWrittenQualifiedNameAsString(const NamedDecl* named_decl) {
   printing_policy.SuppressUnwrittenScope = true;
   named_decl->printQualifiedName(ostream, printing_policy);
 
-  if (const auto* spec =
+  if (const auto* cls_spec =
           dyn_cast<ClassTemplateSpecializationDecl>(named_decl)) {
-    if (spec->isExplicitSpecialization()) {
-      ostream << '<';
-      const TemplateArgumentList& args = spec->getTemplateArgs();
-      const TemplateParameterList* params =
-          spec->getSpecializedTemplate()->getTemplateParameters();
-      for (unsigned i = 0, ie = args.size(); i < ie; ++i) {
-        if (i > 0)
-          ostream << ", ";
-        bool include_type = TemplateParameterList::shouldIncludeTypeForArgument(
-            printing_policy, params, i);
-        const TemplateArgument& arg = args.get(i);
-        // Clang embraces arguments corresponding to a parameter pack in angle
-        // brackets, so cannot use the default 'print' for them.
-        if (arg.getKind() == TemplateArgument::Pack) {
-          ArrayRef<TemplateArgument> pack_args = arg.getPackAsArray();
-          for (size_t j = 0, je = pack_args.size(); j < je; ++j) {
-            if (j > 0)
-              ostream << ", ";
-            pack_args[j].print(printing_policy, ostream, include_type);
-          }
-        } else {
-          args.get(i).print(printing_policy, ostream, include_type);
-        }
-      }
-      ostream << '>';
-
-      // Replace clang placeholders in partial specialization arguments with :N,
-      // where N is the parameter index (depth is ignored for now).
-      static regex tpl_param{"(?:type|value|template)-parameter-\\d+-(\\d+)"};
-      retval = regex_replace(retval, tpl_param, ":$1");
-    }
+    PrintExplicitSpecTplArgs(cls_spec, printing_policy, ostream);
+  } else if (const auto* var_spec =
+                 dyn_cast<VarTemplateSpecializationDecl>(named_decl)) {
+    PrintExplicitSpecTplArgs(var_spec, printing_policy, ostream);
   }
+
+  // Replace clang placeholders in partial specialization arguments with :N,
+  // where N is the parameter index (depth is ignored for now).
+  static regex tpl_param{"(?:type|value|template)-parameter-\\d+-(\\d+)"};
+  retval = regex_replace(retval, tpl_param, ":$1");
 
   return retval;
 }
@@ -1183,34 +1195,43 @@ TemplateInstantiationData GetTplInstDataForVariable(
   return TemplateInstantiationData{resugar_map, provided_types};
 }
 
-const NamedDecl* GetInstantiatedFromDecl(const NamedDecl* class_decl) {
-  if (const ClassTemplateSpecializationDecl* tpl_sp_decl =
-      DynCastFrom(class_decl)) {  // an instantiated class template
+const NamedDecl* GetInstantiatedFromDecl(const NamedDecl* decl) {
+  if (const auto* tpl_sp_decl = dyn_cast<ClassTemplateSpecializationDecl>(
+          decl)) {  // an instantiated class template
     PointerUnion<ClassTemplateDecl*, ClassTemplatePartialSpecializationDecl*>
         instantiated_from = tpl_sp_decl->getInstantiatedFrom();
-    if (const ClassTemplateDecl* tpl_decl =
-        instantiated_from.dyn_cast<ClassTemplateDecl*>()) {
-      // class_decl is instantiated from a non-specialized template.
+    if (const auto* tpl_decl =
+            instantiated_from.dyn_cast<ClassTemplateDecl*>()) {
+      // decl is instantiated from a non-specialized template.
       return tpl_decl;
-    } else if (const ClassTemplatePartialSpecializationDecl*
-               partial_spec_decl =
-               instantiated_from.dyn_cast<
-                   ClassTemplatePartialSpecializationDecl*>()) {
-      // class_decl is instantiated from a template partial specialization.
+    } else if (const auto* partial_spec_decl =
+                   instantiated_from
+                       .dyn_cast<ClassTemplatePartialSpecializationDecl*>()) {
+      // decl is instantiated from a template partial specialization.
       return partial_spec_decl;
     }
   }
-  // class_decl is not instantiated from a template.
-  return class_decl;
+  if (const auto* tpl_sp_decl = dyn_cast<VarTemplateSpecializationDecl>(decl)) {
+    // An instantiated variable template.
+    PointerUnion<VarTemplateDecl*, VarTemplatePartialSpecializationDecl*>
+        instantiated_from = tpl_sp_decl->getInstantiatedFrom();
+    if (const auto* tpl_decl = instantiated_from.dyn_cast<VarTemplateDecl*>()) {
+      // decl is instantiated from a non-specialized template.
+      return tpl_decl;
+    } else if (const auto* partial_spec_decl =
+                   instantiated_from
+                       .dyn_cast<VarTemplatePartialSpecializationDecl*>()) {
+      // decl is instantiated from a template partial specialization.
+      return partial_spec_decl;
+    }
+  }
+  // decl is not instantiated from a class or a variable template.
+  return decl;
 }
 
 const NamedDecl* GetDefinitionAsWritten(const NamedDecl* decl) {
   // First, get to decl-as-written.
-  if (const CXXRecordDecl* class_decl = DynCastFrom(decl)) {
-    decl = GetInstantiatedFromDecl(class_decl);
-    if (const ClassTemplateDecl* tpl_decl = DynCastFrom(decl))
-      decl = tpl_decl->getTemplatedDecl();  // convert back to CXXRecordDecl
-  } else if (const FunctionDecl* func_decl = DynCastFrom(decl)) {
+  if (const auto* func_decl = dyn_cast<FunctionDecl>(decl)) {
     // If we're instantiated from a template, use the template pattern as the
     // decl-as-written.
     // But avoid friend declarations in templates, something happened in Clang
@@ -1219,17 +1240,25 @@ const NamedDecl* GetDefinitionAsWritten(const NamedDecl* decl) {
     const FunctionDecl* tp_decl = func_decl->getTemplateInstantiationPattern();
     if (tp_decl && tp_decl->getFriendObjectKind() == Decl::FOK_None)
       decl = tp_decl;
+  } else {
+    decl = GetInstantiatedFromDecl(decl);
+    if (const auto* tpl_decl = dyn_cast<ClassTemplateDecl>(decl))
+      decl = tpl_decl->getTemplatedDecl();  // convert back to CXXRecordDecl
+    // TODO(bolshakov): getTemplatedDecl() for VarTemplateDecls?
   }
   // Then, get to definition.
   if (const NamedDecl* class_dfn = GetTagDefinition(decl)) {
     return class_dfn;
-  } else if (const FunctionDecl* fn_decl = DynCastFrom(decl)) {
+  } else if (const auto* fn_decl = dyn_cast<FunctionDecl>(decl)) {
     for (FunctionDecl::redecl_iterator it = fn_decl->redecls_begin();
          it != fn_decl->redecls_end(); ++it) {
       if ((*it)->isThisDeclarationADefinition())
         return *it;
     }
   }
+  // For variables, any 'extern' redeclaration is probably sufficient for IWYU
+  // purposes. However, obtaining the definition can be added if needed.
+
   // Couldn't find a definition, just return the original declaration.
   return decl;
 }

tests/cxx/template_spec_mapping-i1.h
@@ -61,3 +61,12 @@ class TplParamPack2 {};
 
 template <typename T, typename... Args>
 class TplParamPack2<int, T, Args...> {};
+
+template <typename>
+constexpr int var_tpl = 1;
+
+template <>
+inline constexpr int var_tpl<char> = 2;
+
+template <typename T>
+constexpr int var_tpl<T*> = 3;

tests/cxx/template_spec_mapping.cc
@@ -63,6 +63,13 @@ TplParamPack2<double, char, float> tpp21;
 // IWYU: TplParamPack2<int, :0, :1...> is...*-i3.h
 TplParamPack2<int, char, float> tpp22;
 
+// IWYU: var_tpl is...*-i2.h
+static_assert(var_tpl<int> == 1);
+// IWYU: var_tpl<char> is...*-i3.h
+static_assert(var_tpl<char> == 2);
+// IWYU: var_tpl<:0 \*> is...*-i4.h
+static_assert(var_tpl<int*> == 3);
+
 /**** IWYU_SUMMARY
 
 tests/cxx/template_spec_mapping.cc should add these lines:
@@ -76,8 +83,8 @@ tests/cxx/template_spec_mapping.cc should remove these lines:
 
 The full include-list for tests/cxx/template_spec_mapping.cc:
 #include "tests/cxx/template_spec_mapping-i1.h"  // for OtherTpl
-#include "tests/cxx/template_spec_mapping-i2.h"  // for Tpl, TplParamPack1, TplParamPack2, TplSpecDistinguishedByIndices, TplWithDeducibleNTTP, TplWithDefArg
-#include "tests/cxx/template_spec_mapping-i3.h"  // for Tpl, TplParamPack1, TplParamPack2, TplSpecDistinguishedByIndices, TplWithDeducibleNTTP, TplWithDefArg
-#include "tests/cxx/template_spec_mapping-i4.h"  // for Tpl, TplParamPack1, TplSpecDistinguishedByIndices, TplWithDeducibleNTTP, TplWithDefArg
+#include "tests/cxx/template_spec_mapping-i2.h"  // for Tpl, TplParamPack1, TplParamPack2, TplSpecDistinguishedByIndices, TplWithDeducibleNTTP, TplWithDefArg, var_tpl
+#include "tests/cxx/template_spec_mapping-i3.h"  // for Tpl, TplParamPack1, TplParamPack2, TplSpecDistinguishedByIndices, TplWithDeducibleNTTP, TplWithDefArg, var_tpl
+#include "tests/cxx/template_spec_mapping-i4.h"  // for Tpl, TplParamPack1, TplSpecDistinguishedByIndices, TplWithDeducibleNTTP, TplWithDefArg, var_tpl
 
 ***** IWYU_SUMMARY */

tests/cxx/template_spec_mapping.imp
@@ -21,4 +21,8 @@
 
   { "symbol": ["TplParamPack2", "private", "\"tests/cxx/template_spec_mapping-i2.h\"", "public"] },
   { "symbol": ["TplParamPack2<int, :0, :1...>", "private", "\"tests/cxx/template_spec_mapping-i3.h\"", "public"] },
+
+  { "symbol": ["var_tpl", "private", "\"tests/cxx/template_spec_mapping-i2.h\"", "public"] },
+  { "symbol": ["var_tpl<char>", "private", "\"tests/cxx/template_spec_mapping-i3.h\"", "public"] },
+  { "symbol": ["var_tpl<:0 *>", "private", "\"tests/cxx/template_spec_mapping-i4.h\"", "public"] },
 ]

commit f81729654195586cc1f9b62b4d044cd03d2727c6

Fix ListSeparator variable name
87ee0ee8151bdf623fd3e22fe0a9c3a403fe7a1a accidentally used LLVM
PascalCase naming for the new ListSeparator, and I missed it in review.

Switch to an IWYU-style name.

iwyu_ast_util.cc
@@ -263,7 +263,7 @@ static void PrintExplicitSpecTplArgs(const T* spec,
     const TemplateArgumentList& args = spec->getTemplateArgs();
     const TemplateParameterList* params =
         spec->getSpecializedTemplate()->getTemplateParameters();
-    ListSeparator LS;  // ', ' by default.
+    ListSeparator sep;  // ', ' by default.
     for (unsigned i = 0, ie = args.size(); i < ie; ++i) {
       bool include_type = TemplateParameterList::shouldIncludeTypeForArgument(
           printing_policy, params, i);
@@ -272,11 +272,11 @@ static void PrintExplicitSpecTplArgs(const T* spec,
       // brackets, so cannot use the default 'print' for them.
       if (arg.getKind() == TemplateArgument::Pack) {
         for (TemplateArgument pack_arg : arg.getPackAsArray()) {
-          ostream << LS;
+          ostream << sep;
           pack_arg.print(printing_policy, ostream, include_type);
         }
       } else {
-        ostream << LS;
+        ostream << sep;
         args.get(i).print(printing_policy, ostream, include_type);
       }
     }