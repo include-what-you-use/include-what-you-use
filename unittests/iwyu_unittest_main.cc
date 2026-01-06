49a4d96b88eaa37c1a0a6d8bb848537f12512c58
Kim Gräsman <kim.grasman@gmail.com>
2026-01-06 17:17:13 +0100

    Set up infrastructure for IWYU unittests
    Add iwyu-unittests and run-unittests targets.
    
    Add a shared iwyu_unittest_main.cc which sets up IWYU globals before
    running tests.
    
    We can now build and run unittests with either of these commands:
    
        ninja iwyu-unittests && ./bin/iwyu-unittests
        ninja iwyu-unittests && ctest [--output-on-failure] -R "unittest"
        ninja run-unittests
    
    Unittests will automatically be picked up in CI by ctest.
    
    No tests registered for execution yet.

---
+++ unittests/iwyu_unittest_main.cc
@@ -0,0 +1,41 @@
+//===--- iwyu_unittest_main.cc - unittest driver --------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#if defined(_WIN32)
+#include <windows.h>
+#if defined(_MSC_VER)
+#include <crtdbg.h>
+#endif
+#endif
+
+#include "gtest/gtest.h"
+
+#include "iwyu_globals.h"
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  include_what_you_use::InitGlobalsAndFlagsForTesting();
+
+  // Shamelessly borrowed from LLVM.
+#if defined(_WIN32)
+  // Disable all of the possible ways Windows conspires to make automated
+  // testing impossible.
+  ::SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
+#if defined(_MSC_VER)
+  ::_set_error_mode(_OUT_TO_STDERR);
+  _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
+  _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
+  _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
+  _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
+  _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
+  _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
+#endif  // defined(_MSC_VER)
+#endif  // defined(_WIN32)
+
+  return RUN_ALL_TESTS();
+}
