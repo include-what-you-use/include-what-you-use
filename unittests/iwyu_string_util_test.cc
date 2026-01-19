04ded5dde03ea917654ec8f263c4c246d1208222
Kim Gräsman <kim.grasman@gmail.com>
2026-01-19 21:22:38 +0100

    Add and clean up iwyu_string_util_test suite

--- unittests/iwyu_string_util_test.cc
+++ unittests/iwyu_string_util_test.cc
@@ -7,28 +7,26 @@
 //
 //===----------------------------------------------------------------------===//
 
-
 // Tests for the iwyu_string_util module.
 
 #include "iwyu_string_util.h"
 
 #include <string>
 
+#include "gtest/gtest.h"
 
-// TODO(dsturtevant): using string for MOE.
+namespace include_what_you_use {
+using std::string;
+using std::vector;
 
-namespace iwyu = include_what_you_use;
-using iwyu::SplitOnWhiteSpace;
-using iwyu::SplitOnWhiteSpacePreservingQuotes;
-using iwyu::StripWhiteSpaceLeft;
-using iwyu::StripWhiteSpaceRight;
-using iwyu::StripWhiteSpace;
+namespace {
 
-#define TEST_OPERATION(op, in, expected_out) { \
-  string str = in; \
-  op(&str); \
-  EXPECT_EQ(string(expected_out), str); \
-}
+#define TEST_OPERATION(op, in, expected_out) \
+  {                                          \
+    string str = in;                         \
+    op(&str);                                \
+    EXPECT_EQ(string(expected_out), str);    \
+  }
 
 TEST(IwyuStringUtilTest, StripWhiteSpaceLeft) {
   TEST_OPERATION(StripWhiteSpaceLeft, "", "");
@@ -58,7 +56,7 @@ TEST(IwyuStringUtilTest, StripWhiteSpace) {
 TEST(IwyuStringUtilTest, SplitOnWhiteSpace) {
   string in = "this is a test";
   vector<string> out = SplitOnWhiteSpace(in, 0);
-  ASSERT_EQ(4, out.size());
+  ASSERT_EQ(size_t(4), out.size());
   EXPECT_EQ(string("this"), out[0]);
   EXPECT_EQ(string("is"), out[1]);
   EXPECT_EQ(string("a"), out[2]);
@@ -66,13 +64,13 @@ TEST(IwyuStringUtilTest, SplitOnWhiteSpace) {
 
   in = "this is a test";
   out = SplitOnWhiteSpace(in, 2);
-  ASSERT_EQ(2, out.size());
+  ASSERT_EQ(size_t(2), out.size());
   EXPECT_EQ(string("this"), out[0]);
   EXPECT_EQ(string("is"), out[1]);
 
   in = "this is\ta    test";
   out = SplitOnWhiteSpace(in, 0);
-  ASSERT_EQ(4, out.size());
+  ASSERT_EQ(size_t(4), out.size());
   EXPECT_EQ(string("this"), out[0]);
   EXPECT_EQ(string("is"), out[1]);
   EXPECT_EQ(string("a"), out[2]);
@@ -80,7 +78,7 @@ TEST(IwyuStringUtilTest, SplitOnWhiteSpace) {
 
   in = " this is a test ";
   out = SplitOnWhiteSpace(in, 0);
-  ASSERT_EQ(4, out.size());
+  ASSERT_EQ(size_t(4), out.size());
   EXPECT_EQ(string("this"), out[0]);
   EXPECT_EQ(string("is"), out[1]);
   EXPECT_EQ(string("a"), out[2]);
@@ -90,33 +88,31 @@ TEST(IwyuStringUtilTest, SplitOnWhiteSpace) {
 TEST(IwyuStringUtilTest, SplitOnWhiteSpacePreservingQuotes) {
   string in = "this is <a test>";
   vector<string> out = SplitOnWhiteSpacePreservingQuotes(in, 0);
-  ASSERT_EQ(3, out.size());
+  ASSERT_EQ(size_t(3), out.size());
   EXPECT_EQ(string("this"), out[0]);
   EXPECT_EQ(string("is"), out[1]);
   EXPECT_EQ(string("<a test>"), out[2]);
 
   in = "this <is a> test";
   out = SplitOnWhiteSpacePreservingQuotes(in, 2);
-  ASSERT_EQ(2, out.size());
+  ASSERT_EQ(size_t(2), out.size());
   EXPECT_EQ(string("this"), out[0]);
   EXPECT_EQ(string("<is a>"), out[1]);
 
   in = "\"this is\"\ta    test";
   out = SplitOnWhiteSpacePreservingQuotes(in, 0);
-  ASSERT_EQ(3, out.size());
+  ASSERT_EQ(size_t(3), out.size());
   EXPECT_EQ(string("\"this is\""), out[0]);
   EXPECT_EQ(string("a"), out[1]);
   EXPECT_EQ(string("test"), out[2]);
 
   in = " this is \"a test\" ";
   out = SplitOnWhiteSpacePreservingQuotes(in, 0);
-  ASSERT_EQ(3, out.size());
+  ASSERT_EQ(size_t(3), out.size());
   EXPECT_EQ(string("this"), out[0]);
   EXPECT_EQ(string("is"), out[1]);
   EXPECT_EQ(string("\"a test\""), out[2]);
 }
 
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+}  // namespace
+}  // namespace include_what_you_use
