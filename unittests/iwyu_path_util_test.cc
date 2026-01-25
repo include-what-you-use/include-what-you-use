32a3b4a04034d3779eb6a4fed042d3a6d75f1bef
Kim Gräsman <kim.grasman@gmail.com>
2026-01-25 12:41:48 +0100

    Move path_util tests into dedicated test suite
    These tests somehow ended up in iwyu_include_picker_test.cc. Create a
    new a suite and move them, so we have a better place to add more tests.

---
+++ unittests/iwyu_path_util_test.cc
@@ -0,0 +1,54 @@
+//===--- iwyu_path_util_test.cc - test iwyu_path_util ------------- -------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// Tests iwyu_path_util.
+
+#include "iwyu_path_util.h"
+
+#include <string>
+
+#include "gtest/gtest.h"
+
+namespace include_what_you_use {
+using std::string;
+
+namespace {
+
+TEST(GetCanonicalName, StripsKnownSuffixes) {
+  EXPECT_EQ("my/path/foo", GetCanonicalName("my/path/foo.cxx"));
+  EXPECT_EQ("my/path/foo", GetCanonicalName("my/path/foo.cpp"));
+  EXPECT_EQ("my/path/foo", GetCanonicalName("my/path/foo.cc"));
+  EXPECT_EQ("my/path/foo", GetCanonicalName("my/path/foo.h"));
+  EXPECT_EQ("my/path/foo", GetCanonicalName("my/path/foo-inl.h"));
+  EXPECT_EQ("my/path/foo", GetCanonicalName("my/path/foo_unittest.cc"));
+  EXPECT_EQ("my/path/foo", GetCanonicalName("my/path/foo_regtest.cc"));
+  EXPECT_EQ("my/path/foo", GetCanonicalName("my/path/foo_test.cc"));
+  EXPECT_EQ("my/path/foo", GetCanonicalName("my/path/foo.c"));
+  EXPECT_EQ("my/path/foo", GetCanonicalName("my/path/foo-inl_unittest.cc"));
+  EXPECT_EQ("my/path/foo_mytest", GetCanonicalName("my/path/foo_mytest.cc"));
+}
+
+TEST(GetCanonicalName, MapsInternalToPublic) {
+  EXPECT_EQ("my/public/foo", GetCanonicalName("my/internal/foo.cc"));
+  EXPECT_EQ("my/public/foo", GetCanonicalName("my/public/foo.cc"));
+  EXPECT_EQ("my/public/foo", GetCanonicalName("my/internal/foo.h"));
+  EXPECT_EQ("my/public/foo", GetCanonicalName("my/public/foo.h"));
+  EXPECT_EQ("internal/foo", GetCanonicalName("internal/foo"));
+  EXPECT_EQ("path/internal_impl", GetCanonicalName("path/internal_impl.cc"));
+}
+
+TEST(ConvertToQuotedInclude, Basic) {
+  EXPECT_EQ("\"foo.h\"", ConvertToQuotedInclude("foo.h"));
+  EXPECT_EQ("<string.h>", ConvertToQuotedInclude("/usr/include/string.h"));
+  EXPECT_EQ("<bits/stl_vector.h>",
+            ConvertToQuotedInclude("/usr/include/c++/4.3/bits/stl_vector.h"));
+}
+
+}  // namespace
+}  // namespace include_what_you_use
