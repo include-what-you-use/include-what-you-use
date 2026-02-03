66639ab878983353c09ddd1caf9dd4aaed40887f
Bolshakov <bolsh.andrey@yandex.ru>
2026-02-03 11:22:30 +0300

    Add standard overloaded functions to mapping
    OVERLOADED_FUNCS set has been kept in the script to minimize changes
    in the mapping. This has a drawback that the set should be maintained
    manually on updating to newer revisions of the standard.

--- tests/cxx/std_symbol_mapping-direct.h
+++ tests/cxx/std_symbol_mapping-direct.h
@@ -8,6 +8,54 @@
 //===----------------------------------------------------------------------===//
 
 namespace std {
-template <class, class>
+typedef decltype(sizeof(int)) size_t;
+
+template <typename, typename>
 struct pair;
+
+template <typename>
+class function;
+template <typename Ret, typename... Args>
+class function<Ret(Args...)>;
+
+template <typename T, size_t N>
+struct array;
+
+template <size_t I, typename T1, typename T2>
+void get(pair<T1, T2>&) noexcept;
+template <size_t I, typename T1, typename T2>
+void get(pair<T1, T2>&&) noexcept;
+template <typename T1, typename T2>
+const T1& get(const pair<T1, T2>& p) noexcept;
+template <typename T2, typename T1>
+const T2& get(const pair<T1, T2>& p) noexcept;
+
+template <size_t I, typename T, size_t N>
+const T&& get(const array<T, N>&&) noexcept;
+
+template <typename T>
+__remove_reference_t(T)&& move(T&&) noexcept;
+
+template <class I, class O>
+O move(I, I, O);
+
+template <typename T>
+void swap(T& x, T& y);
+template <typename T, size_t N>
+void swap(T (&x)[N], T (&y)[N]);
+template <typename Ret, typename... Args>
+void swap(function<Ret(Args...)>&, function<Ret(Args...)>&);
+
+template <typename T>
+class valarray {
+ public:
+  using value_type = T;
+};
+
+template <typename T>
+valarray<T> pow(const valarray<T>&, const typename valarray<T>::value_type&);
+
+float pow(float, float);
+double pow(double, double);
+long double pow(long double, long double);
 }
--- tests/cxx/std_symbol_mapping.cc
+++ tests/cxx/std_symbol_mapping.cc
@@ -21,15 +21,70 @@ template <typename T>
 // IWYU: pair is...*<utility>
 std::pair<T, T> foo();
 
+void Fn() {
+  // IWYU: std::get(std::pair<:1, :2> &) is...*<utility>
+  std::get<0>(*p);
+  // IWYU: std::get(std::pair<:1, :2> &&) is...*<utility>
+  // IWYU: std::move(:0 &&) is...*<utility>
+  std::get<1>(std::move(*p));
+  // IWYU: std::pair is...*<utility>
+  extern const std::pair<int, char> pair2;
+  // IWYU: std::get(const std::pair<:0, :1> &) is...*<utility>
+  (void)std::get<int>(pair2);
+  // IWYU: std::get(const std::pair<:1, :0> &) is...*<utility>
+  (void)std::get<char>(pair2);
+
+  // IWYU: std::array is...*<array>
+  extern const std::array<int, 7> std_arr;
+  // IWYU: std::get(const std::array<:1, :2> &&) is...*<array>
+  // IWYU: std::move(:0 &&) is...*<utility>
+  (void)std::get<1>(std::move(std_arr));
+
+  int arr1[5] = {}, arr2[5] = {}, i1 = 0, i2 = 0;
+  // IWYU: std::move(:0, :0, :1) is...*<algorithm>
+  std::move(arr1, arr1 + 5, arr2);
+
+  // IWYU: std::swap(:0 &, :0 &) is...*<utility>
+  std::swap(i1, i2);
+  // IWYU: std::swap(:0 (&)[:1], :0 (&)[:1]) is...*<utility>
+  std::swap(arr1, arr2);
+
+  // IWYU: std::function is...*<functional>
+  extern std::function<void()> f1, f2;
+  // IWYU: std::swap(std::function<:0 (:1...)> &, std::function<:0 (:1...)> &) is...*<functional>
+  swap(f1, f2);
+
+  // IWYU: std::valarray is...*<valarray>
+  std::valarray<int> va;
+  // IWYU: std::pow(const std::valarray<:0> &, const std::valarray<:0>::value_type &) is...*<valarray>
+  (void)pow(va, 2);
+  // IWYU: std::pow(float, float) is...*<cmath>
+  (void)std::pow(2.0F, 3.0F);
+  // IWYU: std::pow(double, double) is...*<cmath>
+  (void)std::pow(2.0, 3.0);
+  // IWYU: std::pow(long double, long double) is...*<cmath>
+  (void)std::pow(2.0L, 3.0L);
+}
+
 /**** IWYU_SUMMARY
 
 tests/cxx/std_symbol_mapping.cc should add these lines:
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <functional>
 #include <utility>
+#include <valarray>
 
 tests/cxx/std_symbol_mapping.cc should remove these lines:
 - #include "tests/cxx/std_symbol_mapping-direct.h"  // lines XX-XX
 
 The full include-list for tests/cxx/std_symbol_mapping.cc:
-#include <utility>  // for pair
+#include <algorithm>  // for move
+#include <array>  // for array, get
+#include <cmath>  // for pow
+#include <functional>  // for function, swap
+#include <utility>  // for get, move, pair, swap
+#include <valarray>  // for pow, valarray
 
 ***** IWYU_SUMMARY */
