45f5b7c02d3ccfffb3fb4e218a1f34d747ac18b1
Kim Gräsman <kim.grasman@gmail.com>
2026-03-24 19:54:21 +0100

    Add bug test for #1843

---
+++ tests/bugs/1843/tpl.h
@@ -0,0 +1,13 @@
+//===--- tpl.h - iwyu test ------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+template <typename T>
+class Tpl {
+  T t;
+};
