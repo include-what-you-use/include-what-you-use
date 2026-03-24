3f0ce00342056b4c345003b99866faf2366351f8
Kim Gräsman <kim.grasman@gmail.com>
2026-03-24 19:51:38 +0100

    Add bug test for #1822

---
+++ tests/bugs/1822/1822.c
@@ -0,0 +1,23 @@
+//===--- 1822.c - iwyu test -----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// IWYU_XFAIL
+
+#include "shadow.h"
+#include "orig.h"
+
+void foo(void) {
+  call_orig();
+}
+
+/**** IWYU_SUMMARY
+
+(tests/bugs/1822/1822.cc has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
