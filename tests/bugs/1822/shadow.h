4d0ca9bdcbfc6b2d3550dff854a42f60ec31dd68
Kim Gräsman <kim.grasman@gmail.com>
2026-03-24 19:51:38 +0100

    Add bug test for #1822

---
+++ tests/bugs/1822/shadow.h
@@ -0,0 +1,10 @@
+//===--- shadow.h - iwyu test ---------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#define call_orig call_shadow
