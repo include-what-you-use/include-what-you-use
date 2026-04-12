1e302172dbc48a69c66a0c24d9437f1418be059e
Bolshakov <bolsh.andrey@yandex.ru>
2026-04-12 21:44:09 +0300

    Replace directly included headers with canonical
    Canonical headers should be preferred regardless of whether there is
    another public header for a symbol included or not.
    
    Headers mapped to the first public header of a symbol are now also
    considered as canonical ones. This has been done to support "umbrella
    headers". IWYU should honor users' decisions to include e.g.
    `<boost/geometry.hpp>` instead of specific headers.
    
    The check for public headers that are in desired_headers has been moved
    out from the cycle over the uses in `CalculateMinimalIncludes` function
    so that if some of the canonical headers of a symbol is directly
    included, and it is at the same time a public header of some unmapped
    symbol, IWYU attributes the use of the latter to that header, regardless
    of the order of the uses.

---
+++ tests/cxx/canonical_replaces_public-d4.h
@@ -0,0 +1,10 @@
+//===--- canonical_replaces_public-d4.h - test input file for iwyu --------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tests/cxx/canonical_replaces_public-i1.h"
