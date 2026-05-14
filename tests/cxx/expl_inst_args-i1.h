b37561b32d43709d7644323993c6c5718b7ab00f
Kim Gräsman <kim.grasman@gmail.com>
2026-05-14 20:41:57 +0200

    Add test for explicit instantiation declarations/definitions
    https://github.com/llvm/llvm-project/commit/fb024337497 added an
    ExplicitInstantiationDecl in the AST, which affected how Clang visits
    template arguments for explicit instantiations in its default
    RecursiveASTVisitor.
    
    Try to capture our current semantics in a regression test, and mark up
    desired semantics with TODOs. Quite a bit of disconnect currently.
    
    I've checked all TODOs with Clang's behavior as a golden standard.

---
+++ tests/cxx/expl_inst_args-i1.h
@@ -0,0 +1,11 @@
+//===--- expl_inst_args-i1.h - iwyu test ----------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+struct Struct1 {};
+struct Struct2 {};
