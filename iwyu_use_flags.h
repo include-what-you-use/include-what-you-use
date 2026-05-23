cea49968c1d9bea91be8ba6eb2697dc3a08b7567
Kim Gräsman <kim.grasman@gmail.com>
2026-05-23 20:52:06 +0200

    Move UseFlag comments to their own lines
    The trailing line comments make it hard to add a new flag, as doing so
    may cause a reformat of all existing flags when the aligned comments
    don't fit.

--- iwyu_use_flags.h
+++ iwyu_use_flags.h
@@ -15,10 +15,14 @@ namespace include_what_you_use {
 // Flags describing special features of a use that influence IWYU analysis.
 typedef unsigned UseFlags;
 
+// Normal use.
 const UseFlags UF_None = 0;
-const UseFlags UF_InCxxMethodBody = 1;       // use is inside a C++ method body
-const UseFlags UF_RedeclUse = 2;             // use is due to some redeclaration
-const UseFlags UF_ExplicitInstantiation = 4; // use targets an explicit instantiation
+// Use is inside a C++ method body.
+const UseFlags UF_InCxxMethodBody = 1;
+// Use is due to some redeclaration.
+const UseFlags UF_RedeclUse = 2;
+// Use targets an explicit instantiation.
+const UseFlags UF_ExplicitInstantiation = 4;
 }
 
 #endif
