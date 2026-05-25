34099a4c6aa5e6bd89d966186de07e3e5772fbf2
Bolshakov <bolsh.andrey@yandex.ru>
2026-05-25 19:23:05 +0300

    Set fwd-decl context in function types
    Return and parameter types of a function type are forward-declarable
    except inside a function definition.
    
    This fixes #1256.
    
    This does not implement any author-intent policy for function pointers.

--- tests/cxx/expl_inst_args.cc
+++ tests/cxx/expl_inst_args.cc
@@ -62,25 +62,19 @@ using S2Struct3Providing = S2<Struct3>;
 
 // Part 1: add explicit instantiation declarations for these template kinds.
 
-// TODO: Language does not need complete Struct1 here.
 // IWYU: Struct1 needs a declaration
-// IWYU: Struct1 is...*expl_inst_args-i1.h
 extern template void foo<Struct1>(Struct1);              // (a)
 // IWYU: Struct1 needs a declaration
 extern template Struct1 bar<Struct1>;                    // (b)
 // TODO: Language wants complete Struct1 for nested S::Inner.
 // IWYU: Struct1 needs a declaration
 extern template struct S<Struct1>;                       // (c)
-// TODO: Language does not need complete Struct2 here.
 // IWYU: Struct2 needs a declaration
-// IWYU: Struct2 is...*expl_inst_args-i1.h
 extern template void S<Struct2>::method(Struct2);        // (d)
 // IWYU: Struct2 needs a declaration
 extern template Struct2 S<Struct2>::sval;                // (e) & (i)
-// TODO: Language does not need complete Struct2 here.
 // IWYU: Struct1 needs a declaration
 // IWYU: Struct2 needs a declaration
-// IWYU: Struct2 is...*expl_inst_args-i1.h
 extern template void S<Struct1>::tmpl<Struct2>(Struct2); // (f)
 // IWYU: Struct2 needs a declaration
 extern template struct S<Struct2>::Inner;                // (g)
