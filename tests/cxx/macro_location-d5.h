include-what-you-use/include-what-you-use

commit 6601674725703326fc87c832772749ab84e6574d

Keep includes embedded in language constructs
A #include directive may be used not only to inject declarations into
a namespace scope but also to embed an arbitrary sequence of tokens
into some declaration, like this:

    enum E {
    #include "enum-items.h"
    };

This is useful e.g. when the enumeration items are to be autogenerated
by some script. IWYU should keep such includes even if its content is
not declarations or declarations not referred to in the analyzed
translation unit.

This has been achieved by analyzing if an AST node and its direct parent
node are in the same file or not. If the node (after macro expansion) is
in a different file, and the parent node is not a TranslationUnitDecl or
a NamespaceDecl, then IWYU probably deals with an embedded #include.

This should fix #1743.

iwyu.cc
@@ -227,6 +227,7 @@ using clang::MemberExpr;
 using clang::MemberPointerType;
 using clang::NamedDecl;
 using clang::NamespaceAliasDecl;
+using clang::NamespaceDecl;
 using clang::NestedNameSpecifier;
 using clang::NestedNameSpecifierLoc;
 using clang::OpaqueValueExpr;
@@ -5050,6 +5051,26 @@ class IwyuAstConsumer
 
   // --- Visitors of types derived from Decl.
 
+  bool VisitDecl(Decl* decl) {
+    KeepIncludeIfEmbedded();
+    return Base::VisitDecl(decl);
+  }
+
+  bool VisitStmt(Stmt* stmt) {
+    KeepIncludeIfEmbedded();
+    return Base::VisitStmt(stmt);
+  }
+
+  bool VisitType(Type* type) {
+    KeepIncludeIfEmbedded();
+    return Base::VisitType(type);
+  }
+
+  bool VisitTemplateArgumentLoc(const TemplateArgumentLoc& arg) {
+    KeepIncludeIfEmbedded();
+    return Base::VisitTemplateArgumentLoc(arg);
+  }
+
   bool VisitNamespaceAliasDecl(NamespaceAliasDecl* decl) {
     if (CanIgnoreCurrentASTNode())
       return true;
@@ -5855,6 +5876,28 @@ class IwyuAstConsumer
   }
 
  private:
+  // This is to keep includes under non-top-level declarations and other AST
+  // nodes, like in this case:
+  //
+  // int arr[] = {
+  // #include "array_items.h"
+  // };
+  void KeepIncludeIfEmbedded() const {
+    const ASTNode* parent = current_ast_node()->parent();
+    if (!parent || parent->IsA<TranslationUnitDecl>() ||
+        parent->IsA<NamespaceDecl>()) {
+      return;
+    }
+    OptionalFileEntryRef current_file =
+        GetFileEntry(GetInstantiationLoc(CurrentLoc()));
+    OptionalFileEntryRef parent_file = GetFileEntry(parent->GetLocation());
+    if (current_file != parent_file) {
+      preprocessor_info()
+          .FileInfoFor(parent_file)
+          ->ReportKnownDesiredFile(current_file);
+    }
+  }
+
   set<const Type*> GetProvidedTypeComponents(const Type* type) const {
     set<const Type*> res;
     const Type* desugared_until_typedef_or_tpl = Desugar(type);

tests/bugs/1743/enumerators.def
@@ -1,3 +1,0 @@
-FOO,
-BAR,
-BAZ,

tests/cxx/keep_embedded_include-d1.h
@@ -1,4 +1,4 @@
-//===--- 1743.cc - iwyu test ----------------------------------------------===//
+//===--- keep_embedded_include-d1.h - test input file for iwyu ------------===//
 //
 //                     The LLVM Compiler Infrastructure
 //
@@ -7,14 +7,14 @@
 //
 //===----------------------------------------------------------------------===//
 
-// IWYU_XFAIL
-
-enum Foo {
-#include "enumerators.def"
-};
+Item1 =
+#include "tests/cxx/keep_embedded_include-d3.h"
+,
+Item2,
+Item3
 
 /**** IWYU_SUMMARY
 
-(tests/bugs/1743/1743.cc has correct #includes/fwd-decls)
+(tests/cxx/keep_embedded_include-d1.h has correct #includes/fwd-decls)
 
 ***** IWYU_SUMMARY */

tests/cxx/keep_embedded_include-d2.h
@@ -1,0 +1,18 @@
+//===--- keep_embedded_include-d2.h - test input file for iwyu ------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tests/cxx/keep_embedded_include-i1.h"
+
+ITEM
+
+/**** IWYU_SUMMARY
+
+(tests/cxx/keep_embedded_include-d2.h has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */

tests/cxx/keep_embedded_include-d3.h
@@ -1,0 +1,10 @@
+//===--- keep_embedded_include-d3.h - test input file for iwyu ------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+1

tests/cxx/keep_embedded_include-d4.h
@@ -1,0 +1,10 @@
+//===--- keep_embedded_include-d4.h - test input file for iwyu ------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+int

tests/cxx/keep_embedded_include-d5.h
@@ -1,0 +1,10 @@
+//===--- keep_embedded_include-d5.h - test input file for iwyu ------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+int, char, float

tests/cxx/keep_embedded_include-i1.h
@@ -1,0 +1,10 @@
+//===--- keep_embedded_include-i1.h - test input file for iwyu ------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#define ITEM Item

commit b64c22c0c280c47fca1960759b712a2f0b6bb368

Fix KeepIncludeIfEmbedded
This fixes a regression introduced in
6601674725703326fc87c832772749ab84e6574d.

iwyu.cc
@@ -5890,7 +5890,8 @@ class IwyuAstConsumer
     }
     OptionalFileEntryRef current_file =
         GetFileEntry(GetInstantiationLoc(CurrentLoc()));
-    OptionalFileEntryRef parent_file = GetFileEntry(parent->GetLocation());
+    OptionalFileEntryRef parent_file =
+        GetFileEntry(GetInstantiationLoc(parent->GetLocation()));
     if (current_file != parent_file) {
       preprocessor_info()
           .FileInfoFor(parent_file)

tests/cxx/macro_location-d5.h
@@ -1,0 +1,29 @@
+//===--- macro_location-d5.h - test input file for iwyu -------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// This tests that IWYU doesn't require keeping "-d7.h" header here just because
+// an AST node corresponding to '1' is macro-expanded there, whereas its parent
+// BinaryOperator AST node is located in this file. However, this is doubtful:
+// it could make sense to keep "-d7.h" here because it (indirectly) uses
+// the macro TWO defined by includer (i.e. this file).
+
+#define TWO 1 + 1
+
+#include "tests/cxx/macro_location-d7.h"
+
+/**** IWYU_SUMMARY
+
+tests/cxx/macro_location-d5.h should add these lines:
+
+tests/cxx/macro_location-d5.h should remove these lines:
+- #include "tests/cxx/macro_location-d7.h"  // lines XX-XX
+
+The full include-list for tests/cxx/macro_location-d5.h:
+
+***** IWYU_SUMMARY */

tests/cxx/macro_location-d6.h
@@ -1,0 +1,10 @@
+//===--- macro_location-d6.h - test input file for iwyu -------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#define TWO_INDIRECT TWO

tests/cxx/macro_location-d7.h
@@ -1,0 +1,12 @@
+//===--- macro_location-d7.h - test input file for iwyu -------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tests/cxx/macro_location-d6.h"
+
+inline int two = TWO_INDIRECT;

tests/cxx/macro_location.cc
@@ -7,7 +7,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-// IWYU_ARGS: -Xiwyu --check_also="tests/cxx/*-d2.h" -Wno-sizeof-pointer-div \
+// IWYU_ARGS: -Xiwyu --check_also="tests/cxx/*-d2.h" \
+//            -Xiwyu --check_also="tests/cxx/*-d5.h" -Wno-sizeof-pointer-div \
 //            -I .
 
 // Tests a few common types of macro uses to make sure we correctly
@@ -16,6 +17,7 @@
 // expansions when the macro is written in a to-ignore file.
 
 #include "tests/cxx/macro_location.h"
+#include "tests/cxx/macro_location-d5.h"
 #include "tests/cxx/macro_location-inet.h"
 
 struct A {
@@ -40,6 +42,13 @@ int my_htons(int x) {
 
 /**** IWYU_SUMMARY
 
-(tests/cxx/macro_location.cc has correct #includes/fwd-decls)
+tests/cxx/macro_location.cc should add these lines:
+
+tests/cxx/macro_location.cc should remove these lines:
+- #include "tests/cxx/macro_location-d5.h"  // lines XX-XX
+
+The full include-list for tests/cxx/macro_location.cc:
+#include "tests/cxx/macro_location.h"
+#include "tests/cxx/macro_location-inet.h"  // for iwyu_htons
 
 ***** IWYU_SUMMARY */